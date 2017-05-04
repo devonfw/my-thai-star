771d7f7d67effcba95defa25ca34cbd536f70d6b
Pablo Parra <pablo.parra-dominguez@capgemini.com>
2017-05-04 10:48:39 +0200

    added basic structure for the Java backend app with data model integrated

---
+++ java/mtsj/core/src/main/java/io/oasp/application/mtsj/general/common/api/BinaryObject.java
@@ -0,0 +1,33 @@
+package io.oasp.application.mtsj.general.common.api;
+
+/**
+ * This is the interface for a {@link BinaryObject} of the mtsj.
+ *
+ */
+public interface BinaryObject extends ApplicationEntity {
+
+  /**
+   * @param mimeType is the MIME-Type of this {@link BinaryObject}
+   */
+  void setMimeType(String mimeType);
+
+  /**
+   * Returns MIME-Type of thie {@link BinaryObject}
+   *
+   * @return the MIME-Type, e.g image/jpeg
+   */
+  String getMimeType();
+
+  /**
+   * @return Returns the size in bytes
+   */
+  long getSize();
+
+  /**
+   * Sets the size of bytes
+   *
+   * @param size the size in bytes
+   */
+  void setSize(long size);
+
+}
---
+++ java/mtsj/core/src/main/java/io/oasp/application/mtsj/general/common/base/AbstractBeanMapperSupport.java
@@ -0,0 +1,33 @@
+package io.oasp.application.mtsj.general.common.base;
+
+import io.oasp.module.beanmapping.common.api.BeanMapper;
+
+import javax.inject.Inject;
+
+/**
+ * This abstract class provides {@link #getBeanMapper() access} to the {@link BeanMapper}.
+ *
+ */
+public abstract class AbstractBeanMapperSupport {
+
+  /** @see #getBeanMapper() */
+  private BeanMapper beanMapper;
+
+  /**
+   * @param beanMapper is the {@link BeanMapper} to {@link Inject}
+   */
+  @Inject
+  public void setBeanMapper(BeanMapper beanMapper) {
+
+    this.beanMapper = beanMapper;
+  }
+
+  /**
+   * @return the {@link BeanMapper} instance.
+   */
+  protected BeanMapper getBeanMapper() {
+
+    return this.beanMapper;
+  }
+
+}
