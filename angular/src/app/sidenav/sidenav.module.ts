6df5ac18aac4772df837fe80160ae588a923690d
Bartlomiej Pietrowski <bartlomiej.pietrowski@capgemini.com>
2017-05-09 11:44:58 +0200

    Created module for sidenav related code - first step to clean app.module

---
+++ angular/src/app/sidenav/sidenav.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@angular/material';
+
+import { SidenavService } from './shared/sidenav.service';
+import { PriceCalculatorService } from './shared/price-calculator.service';
+import { SidenavComponent } from './sidenav.component';
+import { SidenavOrderComponent } from './sidenav-order/sidenav-order.component';
+import { CommentDialogComponent } from './comment-dialog/comment-dialog.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+  ],
+  providers: [
+    SidenavService,
+    PriceCalculatorService,
+  ],
+  declarations: [
+    SidenavComponent,
+    SidenavOrderComponent,
+    CommentDialogComponent,
+  ],
+  exports: [
+    SidenavComponent,
+  ],
+  entryComponents: [
+    CommentDialogComponent,
+  ],
+})
+export class SidenavModule { }
