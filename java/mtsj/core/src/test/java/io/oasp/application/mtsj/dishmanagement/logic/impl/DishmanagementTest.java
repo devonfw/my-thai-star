b6459d44abbdda671186685f725f1cd4ebacc39a
Pablo Parra <pablo.parra-dominguez@capgemini.com>
2017-05-26 16:09:17 +0200

    Fixed Dishmanagement and Ordermanagement unit tests

--- java/mtsj/core/src/test/java/io/oasp/application/mtsj/dishmanagement/logic/impl/DishmanagementTest.java
+++ java/mtsj/core/src/test/java/io/oasp/application/mtsj/dishmanagement/logic/impl/DishmanagementTest.java
@@ -1,12 +1,20 @@
 package io.oasp.application.mtsj.dishmanagement.logic.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.junit.Test;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import io.oasp.application.mtsj.SpringBootApp;
 import io.oasp.application.mtsj.dishmanagement.logic.api.Dishmanagement;
+import io.oasp.application.mtsj.dishmanagement.logic.api.to.CategoryEto;
+import io.oasp.application.mtsj.dishmanagement.logic.api.to.DishCto;
+import io.oasp.application.mtsj.dishmanagement.logic.api.to.DishSearchCriteriaTo;
+import io.oasp.module.jpa.common.api.to.PaginatedListTo;
 import io.oasp.module.test.common.base.ComponentTest;
 
 /**
@@ -18,36 +26,36 @@ import io.oasp.module.test.common.base.ComponentTest;
 public class DishmanagementTest extends ComponentTest {
 
   @Inject
-  private Dishmanagement platemanagement;
+  private Dishmanagement dishmanagement;
 
   /**
-   * This test gets all the available platees using an empty SearchCriteria object
+   * This test gets all the available dishes using an empty SearchCriteria object
    */
-  // @Test
-  // public void findAllPlates() {
-  //
-  // DishSearchCriteriaTo criteria = new DishSearchCriteriaTo();
-  // List<CategoryEntity> categories = new ArrayList<>();
-  // criteria.setCategories(categories);
-  // PaginatedListTo<DishEto> result = this.platemanagement.findDishEtos(criteria);
-  // assertThat(result).isNotNull();
-  // }
-  //
-  // /**
-  // * This test filters all the available platees that match the SearchCriteria object
-  // */
-  // @Test
-  // public void filterPlates() {
-  //
-  // DishSearchCriteriaTo criteria = new DishSearchCriteriaTo();
-  // List<CategoryEntity> categories = new ArrayList<>();
-  // criteria.setCategories(categories);
-  // criteria.setSearchBy("Garlic Paradise");
-  // PaginatedListTo<DishEto> result = this.platemanagement.findDishEtos(criteria);
-  //
-  // assertThat(result).isNotNull();
-  // assertThat(result.getResult().size()).isGreaterThan(0);
-  // assertThat(result.getResult().get(0).getId()).isEqualTo(1L);
-  // }
+  @Test
+  public void findAllDishes() {
+
+    DishSearchCriteriaTo criteria = new DishSearchCriteriaTo();
+    List<CategoryEto> categories = new ArrayList<>();
+    criteria.setCategories(categories);
+    PaginatedListTo<DishCto> result = this.dishmanagement.findDishCtos(criteria);
+    assertThat(result).isNotNull();
+  }
+
+  /**
+   * This test filters all the available dishes that match the SearchCriteria object
+   */
+  @Test
+  public void filterDishes() {
+
+    DishSearchCriteriaTo criteria = new DishSearchCriteriaTo();
+    List<CategoryEto> categories = new ArrayList<>();
+    criteria.setCategories(categories);
+    criteria.setSearchBy("Garlic Paradise");
+    PaginatedListTo<DishCto> result = this.dishmanagement.findDishCtos(criteria);
+
+    assertThat(result).isNotNull();
+    assertThat(result.getResult().size()).isGreaterThan(0);
+    assertThat(result.getResult().get(0).getDish().getId()).isEqualTo(1L);
+  }
 
 }
--- java/mtsj/core/src/test/java/io/oasp/application/mtsj/ordermanagement/logic/impl/OrdermanagementTest.java
+++ java/mtsj/core/src/test/java/io/oasp/application/mtsj/ordermanagement/logic/impl/OrdermanagementTest.java
@@ -1,124 +1,173 @@
 package io.oasp.application.mtsj.ordermanagement.logic.impl;
 
-/// **
-// * Test for {@link Ordermanagement}
-// *
-// */
-// @SpringApplicationConfiguration(classes = SpringBootApp.class)
-// @WebAppConfiguration
-// public class OrdermanagementTest extends ComponentTest {
-//
-// @Inject
-// private Ordermanagement orderManagement;
-//
-// OrderEto orderEto;
-//
-// /**
-// * Creation of needed objects
-// */
-// @Override
-// public void doSetUp() {
-//
-// // extra ingredients
-// Ingredient i1 = new Ingredient();
-// i1.setId(0L);
-// Ingredient i2 = new Ingredient();
-// i2.setId(1L);
-// List<Ingredient> extras = new ArrayList<>();
-// extras.add(i1);
-// extras.add(i2);
-//
-// // order line 1
-// OrderLineEntity ol1 = new OrderLineEntity();
-// ol1.setIdDish(5L);
-// ol1.setExtras(extras);
-// ol1.setAmount(1);
-// ol1.setComment("This is a test order line");
-//
-// // order line 1
-// OrderLineEntity ol2 = new OrderLineEntity();
-// ol2.setIdDish(5L);
-// ol2.setExtras(new ArrayList<Ingredient>());
-// ol2.setAmount(1);
-// ol2.setComment("This is another order line");
-//
-// // order
-// List<OrderLineEntity> lines = new ArrayList<>();
-// lines.add(ol1);
-// lines.add(ol2);
-//
-// this.orderEto = new OrderEto();
-// this.orderEto.setToken("CB_20170510_123502595Z");
-// this.orderEto.setLines(lines);
-// }
-//
-// /**
-// * Tests if an order is created
-// */
-// @Test
-// public void orderAnOrder() {
-//
-// OrderEto createdOrder = this.orderManagement.saveOrder(this.orderEto);
-// assertThat(createdOrder).isNotNull();
-// }
-//
-// /**
-// * Tests that an order with a wrong token is not created
-// */
-// @Test
-// public void orderAnOrderWithWrongToken() {
-//
-// this.orderEto.setToken("wrongToken");
-// try {
-// this.orderManagement.saveOrder(this.orderEto);
-// } catch (Exception e) {
-// WrongTokenException wte = new WrongTokenException();
-// assertThat(e.getClass()).isEqualTo(wte.getClass());
-// }
-// }
-//
-// /**
-// * Tests that an already created order is not created again
-// */
-// @Test
-// public void orderAnOrderAlreadyCreated() {
-//
-// this.orderEto.setToken("CB_20170509_123502555Z");
-// try {
-// this.orderManagement.saveOrder(this.orderEto);
-// } catch (Exception e) {
-// OrderAlreadyExistException oae = new OrderAlreadyExistException();
-// assertThat(e.getClass()).isEqualTo(oae.getClass());
-// }
-// }
-//
-// /**
-// * Tests that an order with a booking token that does not exist is not created
-// */
-// @Test
-// public void orderAnOrderBookingNotExist() {
-//
-// this.orderEto.setToken("CB_Not_Existing_Token");
-// try {
-// this.orderManagement.saveOrder(this.orderEto);
-// } catch (Exception e) {
-// NoBookingException nb = new NoBookingException();
-// assertThat(e.getClass()).isEqualTo(nb.getClass());
-// }
-// }
-//
-// /**
-// * Tests that an order with a guest token that does not exist is not created
-// */
-// @Test
-// public void orderAnOrderInviteNotExist() {
-//
-// this.orderEto.setToken("GB_Not_Existing_Token");
-// try {
-// this.orderManagement.saveOrder(this.orderEto);
-// } catch (Exception e) {
-// NoInviteException ni = new NoInviteException();
-// assertThat(e.getClass()).isEqualTo(ni.getClass());
-// }
-// }
-// }
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import io.oasp.application.mtsj.SpringBootApp;
+import io.oasp.application.mtsj.bookingmanagement.logic.api.to.BookingEto;
+import io.oasp.application.mtsj.dishmanagement.dataaccess.api.IngredientEntity;
+import io.oasp.application.mtsj.dishmanagement.logic.api.to.DishEto;
+import io.oasp.application.mtsj.ordermanagement.common.api.exception.NoBookingException;
+import io.oasp.application.mtsj.ordermanagement.common.api.exception.NoInviteException;
+import io.oasp.application.mtsj.ordermanagement.common.api.exception.OrderAlreadyExistException;
+import io.oasp.application.mtsj.ordermanagement.common.api.exception.WrongTokenException;
+import io.oasp.application.mtsj.ordermanagement.logic.api.Ordermanagement;
+import io.oasp.application.mtsj.ordermanagement.logic.api.to.OrderCto;
+import io.oasp.application.mtsj.ordermanagement.logic.api.to.OrderEto;
+import io.oasp.application.mtsj.ordermanagement.logic.api.to.OrderLineCto;
+import io.oasp.application.mtsj.ordermanagement.logic.api.to.OrderLineEto;
+import io.oasp.module.test.common.base.ComponentTest;
+
+/**
+ * Test for {@link Ordermanagement}
+ *
+ */
+@SpringApplicationConfiguration(classes = SpringBootApp.class)
+@WebAppConfiguration
+public class OrdermanagementTest extends ComponentTest {
+
+  @Inject
+  private Ordermanagement orderManagement;
+
+  OrderCto orderCto;
+
+  /**
+   * Creation of needed objects
+   */
+  @Override
+  public void doSetUp() {
+
+    // extra ingredients
+    IngredientEntity i1 = new IngredientEntity();
+    i1.setId(0L);
+    IngredientEntity i2 = new IngredientEntity();
+    i2.setId(1L);
+    List<IngredientEntity> extras = new ArrayList<>();
+    extras.add(i1);
+    extras.add(i2);
+
+    // Dish
+    DishEto dishEto = new DishEto();
+    dishEto.setId(5L);
+
+    // OrderLine Eto 1
+    OrderLineEto olEto1 = new OrderLineEto();
+    olEto1.setAmount(3);
+    olEto1.setComment("This is a test order line");
+    olEto1.setDishId(dishEto.getId());
+
+    // OrderLine Eto 2
+    OrderLineEto olEto2 = new OrderLineEto();
+    olEto2.setAmount(1);
+    olEto2.setComment("This is another order line");
+    olEto2.setDishId(dishEto.getId());
+
+    // order line 1
+    OrderLineCto ol1 = new OrderLineCto();
+    ol1.setDish(dishEto);
+    ol1.setExtras(extras);
+    ol1.setOrderLine(olEto1);
+
+    // order line 2
+    OrderLineCto ol2 = new OrderLineCto();
+    ol2.setDish(dishEto);
+    ol2.setExtras(new ArrayList<IngredientEntity>());
+    ol1.setOrderLine(olEto2);
+
+    // order
+    List<OrderLineCto> lines = new ArrayList<>();
+    lines.add(ol1);
+    // lines.add(ol2);
+
+    BookingEto bookingEto = new BookingEto();
+    bookingEto.setBookingToken("CB_20170510_123502595Z");
+    this.orderCto = new OrderCto();
+    this.orderCto.setBooking(bookingEto);
+    this.orderCto.setOrderLines(lines);
+
+  }
+
+  /**
+   * Tests if an order is created
+   */
+  @Test
+  public void orderAnOrder() {
+
+    OrderEto createdOrder = this.orderManagement.saveOrder(this.orderCto);
+    assertThat(createdOrder).isNotNull();
+  }
+
+  /**
+   * Tests that an order with a wrong token is not created
+   */
+  @Test
+  public void orderAnOrderWithWrongToken() {
+
+    BookingEto bookingEto = new BookingEto();
+    bookingEto.setBookingToken("wrongToken");
+    this.orderCto.setBooking(bookingEto);
+    try {
+      this.orderManagement.saveOrder(this.orderCto);
+    } catch (Exception e) {
+      WrongTokenException wte = new WrongTokenException();
+      assertThat(e.getClass()).isEqualTo(wte.getClass());
+    }
+  }
+
+  /**
+   * Tests that an already created order is not created again
+   */
+  @Test
+  public void orderAnOrderAlreadyCreated() {
+
+    BookingEto bookingEto = new BookingEto();
+    bookingEto.setBookingToken("CB_20170509_123502555Z");
+    this.orderCto.setBooking(bookingEto);
+    try {
+      this.orderManagement.saveOrder(this.orderCto);
+    } catch (Exception e) {
+      OrderAlreadyExistException oae = new OrderAlreadyExistException();
+      assertThat(e.getClass()).isEqualTo(oae.getClass());
+    }
+  }
+
+  /**
+   * Tests that an order with a booking token that does not exist is not created
+   */
+  @Test
+  public void orderAnOrderBookingNotExist() {
+
+    BookingEto bookingEto = new BookingEto();
+    bookingEto.setBookingToken("CB_Not_Existing_Token");
+    this.orderCto.setBooking(bookingEto);
+    try {
+      this.orderManagement.saveOrder(this.orderCto);
+    } catch (Exception e) {
+      NoBookingException nb = new NoBookingException();
+      assertThat(e.getClass()).isEqualTo(nb.getClass());
+    }
+  }
+
+  /**
+   * Tests that an order with a guest token that does not exist is not created
+   */
+  @Test
+  public void orderAnOrderInviteNotExist() {
+
+    BookingEto bookingEto = new BookingEto();
+    bookingEto.setBookingToken("GB_Not_Existing_Token");
+    this.orderCto.setBooking(bookingEto);
+    try {
+      this.orderManagement.saveOrder(this.orderCto);
+    } catch (Exception e) {
+      NoInviteException ni = new NoInviteException();
+      assertThat(e.getClass()).isEqualTo(ni.getClass());
+    }
+  }
+}
