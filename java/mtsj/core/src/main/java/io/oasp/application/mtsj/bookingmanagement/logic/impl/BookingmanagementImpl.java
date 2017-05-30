fc046d379207737bc27e6722bc0bdcd2742809d7
Ruben Diaz <ruben.diaz-martinez@capgemini.com>
2017-05-30 17:33:48 +0200

    updated MD5 codification process for tokens

--- java/mtsj/core/src/main/java/io/oasp/application/mtsj/bookingmanagement/logic/impl/BookingmanagementImpl.java
+++ java/mtsj/core/src/main/java/io/oasp/application/mtsj/bookingmanagement/logic/impl/BookingmanagementImpl.java
@@ -185,14 +185,13 @@ public class BookingmanagementImpl extends AbstractComponentFacade implements Bo
         + String.format("%02d", ldt1.getSecond());
 
     MessageDigest md = MessageDigest.getInstance("MD5");
-    md.update((type + email + date + time).getBytes());
+    md.update((email + date + time).getBytes());
     byte[] digest = md.digest();
     StringBuilder sb = new StringBuilder();
     for (byte b : digest) {
       sb.append(String.format("%02x", b & 0xff));
     }
-    System.out.println(sb.toString());
-    return sb.toString();
+    return type + date + sb;
   }
 
   /**
