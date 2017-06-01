f78d49a31623f736f4dcf106dd635dfe38455e31
Pablo Parra <pablo.parra-dominguez@capgemini.com>
2017-06-01 15:29:19 +0200

    Added JWT token generation with credentials

---
+++ java/mtsj/core/src/main/java/io/oasp/application/mtsj/general/security/JWTLoginFilter.java
@@ -0,0 +1,41 @@
+package io.oasp.application.mtsj.general.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+  public JWTLoginFilter(String url, AuthenticationManager authManager) {
+    super(new AntPathRequestMatcher(url));
+    setAuthenticationManager(authManager);
+  }
+
+  @Override
+  public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+      throws AuthenticationException, IOException, ServletException {
+
+    AccountCredentials creds = new ObjectMapper().readValue(req.getInputStream(), AccountCredentials.class);
+    return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(creds.getUsername(),
+        creds.getPassword()/* , Collections.emptyList() */));
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+      Authentication auth) throws IOException, ServletException {
+
+    TokenAuthenticationService.addAuthentication(res, auth.getName());
+  }
+}
--- java/mtsj/core/src/main/java/io/oasp/application/mtsj/general/service/impl/config/BaseWebSecurityConfig.java
+++ java/mtsj/core/src/main/java/io/oasp/application/mtsj/general/service/impl/config/BaseWebSecurityConfig.java
@@ -1,27 +1,15 @@
 package io.oasp.application.mtsj.general.service.impl.config;
 
-import javax.inject.Inject;
-import javax.servlet.Filter;
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import io.oasp.module.security.common.impl.rest.AuthenticationSuccessHandlerSendingOkHttpStatusCode;
+import io.oasp.application.mtsj.general.security.JWTAuthenticationFilter;
+import io.oasp.application.mtsj.general.security.JWTLoginFilter;
 import io.oasp.module.security.common.impl.rest.JsonUsernamePasswordAuthenticationFilter;
-import io.oasp.module.security.common.impl.rest.LogoutSuccessHandlerReturningOkHttpStatusCode;
 
 /**
  * This type serves as a base class for extensions of the {@code WebSecurityConfigurerAdapter} and provides a default
@@ -34,26 +22,26 @@ public abstract class BaseWebSecurityConfig extends WebSecurityConfigurerAdapter
   @Value("${security.cors.enabled}")
   boolean corsEnabled = true;
 
-  @Inject
-  private UserDetailsService userDetailsService;
+  // @Inject
+  // private UserDetailsService userDetailsService;
 
-  private CorsFilter getCorsFilter() {
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    config.addAllowedOrigin("*");
-    config.addAllowedHeader("*");
-    config.addAllowedMethod("OPTIONS");
-    config.addAllowedMethod("HEAD");
-    config.addAllowedMethod("GET");
-    config.addAllowedMethod("PUT");
-    config.addAllowedMethod("POST");
-    config.addAllowedMethod("DELETE");
-    config.addAllowedMethod("PATCH");
-    source.registerCorsConfiguration("/**", config);
-    return new CorsFilter(source);
-  }
+  // private CorsFilter getCorsFilter() {
+  //
+  // UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+  // CorsConfiguration config = new CorsConfiguration();
+  // config.setAllowCredentials(true);
+  // config.addAllowedOrigin("*");
+  // config.addAllowedHeader("*");
+  // config.addAllowedMethod("OPTIONS");
+  // config.addAllowedMethod("HEAD");
+  // config.addAllowedMethod("GET");
+  // config.addAllowedMethod("PUT");
+  // config.addAllowedMethod("POST");
+  // config.addAllowedMethod("DELETE");
+  // config.addAllowedMethod("PATCH");
+  // source.registerCorsConfiguration("/**", config);
+  // return new CorsFilter(source);
+  // }
 
   /**
    * Configure spring security to enable a simple webform-login + a simple rest login.
@@ -61,31 +49,39 @@ public abstract class BaseWebSecurityConfig extends WebSecurityConfigurerAdapter
   @Override
   public void configure(HttpSecurity http) throws Exception {
 
-    String[] unsecuredResources =
-        new String[] { "/login", "/security/**", "/services/rest/login", "/services/rest/logout", "/services/rest/**" };
-    http
-        //
-        .userDetailsService(this.userDetailsService)
-        // define all urls that are not to be secured
-        .authorizeRequests().antMatchers(unsecuredResources).permitAll().anyRequest().authenticated().and()
+    String[] unsecuredResources = new String[] { "/login", "/security/**", "/services/rest/login",
+    "/services/rest/logout", /* "/services/rest/**" */"/services/rest/bookingmanagement/**" };
+    // http
+    // //
+    // .userDetailsService(this.userDetailsService)
+    // // define all urls that are not to be secured
+    // .authorizeRequests().antMatchers(unsecuredResources).permitAll().anyRequest().authenticated().and()
+    //
+    // // activate crsf check for a selection of urls (but not for login & logout)
+    // // .csrf().requireCsrfProtectionMatcher(new CsrfRequestMatcher()).and()
+    // .csrf().disable()
+    // // configure parameters for simple form login (and logout)
+    // .formLogin().successHandler(new SimpleUrlAuthenticationSuccessHandler()).defaultSuccessUrl("/")
+    // .failureUrl("/login.html?error").loginProcessingUrl("/j_spring_security_login").usernameParameter("username")
+    // .passwordParameter("password").and()
+    // // logout via POST is possible
+    // .logout().logoutSuccessUrl("/login.html").and()
+    //
+    // // register login and logout filter that handles rest logins
+    // .addFilterAfter(getSimpleRestAuthenticationFilter(), BasicAuthenticationFilter.class)
+    // .addFilterAfter(getSimpleRestLogoutFilter(), LogoutFilter.class);
 
-        // activate crsf check for a selection of urls (but not for login & logout)
-        // .csrf().requireCsrfProtectionMatcher(new CsrfRequestMatcher()).and()
-        .csrf().disable()
-        // configure parameters for simple form login (and logout)
-        .formLogin().successHandler(new SimpleUrlAuthenticationSuccessHandler()).defaultSuccessUrl("/")
-        .failureUrl("/login.html?error").loginProcessingUrl("/j_spring_security_login").usernameParameter("username")
-        .passwordParameter("password").and()
-        // logout via POST is possible
-        .logout().logoutSuccessUrl("/login.html").and()
+    http.csrf().disable().authorizeRequests().antMatchers(unsecuredResources).permitAll()
+        .antMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated().and()
+        // We filter the api/login requests
+        .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+            UsernamePasswordAuthenticationFilter.class)
+        // And filter other requests to check the presence of JWT in header
+        .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        // register login and logout filter that handles rest logins
-        .addFilterAfter(getSimpleRestAuthenticationFilter(), BasicAuthenticationFilter.class)
-        .addFilterAfter(getSimpleRestLogoutFilter(), LogoutFilter.class);
-
-    if (this.corsEnabled) {
-      http.addFilterBefore(getCorsFilter(), CsrfFilter.class);
-    }
+    // if (this.corsEnabled) {
+    // http.addFilterBefore(getCorsFilter(), CsrfFilter.class);
+    // }
   }
 
   /**
@@ -94,16 +90,16 @@ public abstract class BaseWebSecurityConfig extends WebSecurityConfigurerAdapter
    *
    * @return the filter.
    */
-  protected Filter getSimpleRestLogoutFilter() {
-
-    LogoutFilter logoutFilter =
-        new LogoutFilter(new LogoutSuccessHandlerReturningOkHttpStatusCode(), new SecurityContextLogoutHandler());
-
-    // configure logout for rest logouts
-    logoutFilter.setLogoutRequestMatcher(new AntPathRequestMatcher("/services/rest/logout"));
-
-    return logoutFilter;
-  }
+  // protected Filter getSimpleRestLogoutFilter() {
+  //
+  // LogoutFilter logoutFilter =
+  // new LogoutFilter(new LogoutSuccessHandlerReturningOkHttpStatusCode(), new SecurityContextLogoutHandler());
+  //
+  // // configure logout for rest logouts
+  // logoutFilter.setLogoutRequestMatcher(new AntPathRequestMatcher("/services/rest/logout"));
+  //
+  // return logoutFilter;
+  // }
 
   /**
    * Create a simple authentication filter for REST logins that reads user-credentials from a json-parameter and returns
@@ -112,28 +108,28 @@ public abstract class BaseWebSecurityConfig extends WebSecurityConfigurerAdapter
    * @return the {@link JsonUsernamePasswordAuthenticationFilter}.
    * @throws Exception if something goes wrong.
    */
-  protected JsonUsernamePasswordAuthenticationFilter getSimpleRestAuthenticationFilter() throws Exception {
+  // protected JsonUsernamePasswordAuthenticationFilter getSimpleRestAuthenticationFilter() throws Exception {
+  //
+  // JsonUsernamePasswordAuthenticationFilter jsonFilter =
+  // new JsonUsernamePasswordAuthenticationFilter(new AntPathRequestMatcher("/services/rest/login"));
+  // jsonFilter.setPasswordParameter("j_password");
+  // jsonFilter.setUsernameParameter("j_username");
+  // jsonFilter.setAuthenticationManager(authenticationManager());
+  // // set failurehandler that uses no redirect in case of login failure; just HTTP-status: 401
+  // jsonFilter.setAuthenticationManager(authenticationManagerBean());
+  // jsonFilter.setAuthenticationFailureHandler(new SimpleUrlAuthenticationFailureHandler());
+  // // set successhandler that uses no redirect in case of login success; just HTTP-status: 200
+  // jsonFilter.setAuthenticationSuccessHandler(new AuthenticationSuccessHandlerSendingOkHttpStatusCode());
+  // return jsonFilter;
+  // }
 
-    JsonUsernamePasswordAuthenticationFilter jsonFilter =
-        new JsonUsernamePasswordAuthenticationFilter(new AntPathRequestMatcher("/services/rest/login"));
-    jsonFilter.setPasswordParameter("j_password");
-    jsonFilter.setUsernameParameter("j_username");
-    jsonFilter.setAuthenticationManager(authenticationManager());
-    // set failurehandler that uses no redirect in case of login failure; just HTTP-status: 401
-    jsonFilter.setAuthenticationManager(authenticationManagerBean());
-    jsonFilter.setAuthenticationFailureHandler(new SimpleUrlAuthenticationFailureHandler());
-    // set successhandler that uses no redirect in case of login success; just HTTP-status: 200
-    jsonFilter.setAuthenticationSuccessHandler(new AuthenticationSuccessHandlerSendingOkHttpStatusCode());
-    return jsonFilter;
-  }
-
+  @Override
   @SuppressWarnings("javadoc")
-  @Inject
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+  // @Inject
+  public void configure/* Global */(AuthenticationManagerBuilder auth) throws Exception {
 
-    auth.inMemoryAuthentication().withUser("waiter").password("waiter").roles("Waiter").and().withUser("cook")
-        .password("cook").roles("Cook").and().withUser("barkeeper").password("barkeeper").roles("Barkeeper").and()
-        .withUser("chief").password("chief").roles("Chief");
+    auth.inMemoryAuthentication().withUser("waiter").password("waiter").roles("Waiter").and().withUser("chief")
+        .password("chief").roles("Chief");
   }
 
 }
