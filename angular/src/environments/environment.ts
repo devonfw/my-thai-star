ca641115388d40e1b28e1feed542fa48edd9774f
Belda Perez <cristobal.belda-perez@capgemini.com>
2017-06-16 14:22:09 +0200

    angular testing addapted to environments

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,12 +3,15 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-import { BackendType } from '../app/shared/backend/backend.module';
+export enum BackendType {
+  IN_MEMORY,
+  REST,
+  GRAPHQL,
+}
 
-export const environment: {production: boolean, backendType: BackendType, envName: string, restPathRoot: string, restServiceRoot: string} = {
+export const environment: {production: boolean, backendType: BackendType, restPathRoot: string, restServiceRoot: string} = {
   production: false,
-  backendType: BackendType.REST,
-  envName: 'dev',
+  backendType: BackendType.REST, // BackendType.REST (second value)
   restPathRoot: 'http://localhost:8081/mythaistar/',
   restServiceRoot: 'http://localhost:8081/mythaistar/services/rest/',
 };
