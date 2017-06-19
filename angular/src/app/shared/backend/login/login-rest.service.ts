53ef81186d7548bb5948cfe85a728dd9189982b3
Belda Perez <cristobal.belda-perez@capgemini.com>
2017-06-19 13:34:44 +0200

    angular: rest services pointing to correct API endpoints

--- angular/src/app/shared/backend/login/login-rest.service.ts
+++ angular/src/app/shared/backend/login/login-rest.service.ts
@@ -23,21 +23,21 @@ export class LoginRestService implements ILoginDataService {
   }
 
   login(username: string, password: string): Observable<string> {
-    return this.http.post(`${config.restPathRoot}${this.loginRestPath}`, {username: username, password: password});
+    return this.http.post(`${environment.restPathRoot}${this.loginRestPath}`, {username: username, password: password});
   }
 
   getCurrentUser(): Observable<LoginInfo> {
-    return this.http.get(`${config.restServiceRoot}${this.currentUserRestPath}`)
+    return this.http.get(`${environment.restServiceRoot}${this.currentUserRestPath}`)
       .map((res: Response) => res.json());
   }
 
   register(email: string, password: string): Observable<LoginInfo> {
-    return this.http.post(`${config.restServiceRoot}${this.registerRestPath}`, {email: email, password: password})
+    return this.http.post(`${environment.restServiceRoot}${this.registerRestPath}`, {email: email, password: password})
       .map((res: Response) => res.json());
   }
 
   changePassword(username: string, oldPassword: string, newPassword: string): Observable<any> {
-    return this.http.post(`${config.restServiceRoot}${this.changePasswordRestPath}`,
+    return this.http.post(`${environment.restServiceRoot}${this.changePasswordRestPath}`,
         {username: username, oldPassword: oldPassword, newPassword: newPassword},
       )
       .map((res: Response) => res.json());
