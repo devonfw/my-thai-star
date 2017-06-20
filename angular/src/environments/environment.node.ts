21003955c02fbd98ef2d253b793d264b7784e075
Belda Perez <cristobal.belda-perez@capgemini.com>
2017-06-20 12:20:12 +0200

    new node environment for angular client

--- angular/src/environments/environment.node.ts
+++ angular/src/environments/environment.node.ts
@@ -7,6 +7,6 @@ export enum BackendType {
 export const environment: {production: boolean, backendType: BackendType, restPathRoot: string, restServiceRoot: string} = {
   production: false,
   backendType: BackendType.REST,
-  restPathRoot: 'http://de-mucdevondepl01:9091/mythaistar/',
-  restServiceRoot: 'http://de-mucdevondepl01:9091/mythaistar/services/rest/',
+  restPathRoot: 'http://localhost:9080/mythaistar/',
+  restServiceRoot: 'http://localhost:9080/mythaistar/services/rest/',
 };
