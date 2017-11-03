9bab4e7e67d7c868d0660ab5955106245eab7923
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2017-11-03 13:20:06 +0100

    Routing fixed for email confirmations.

--- angular/src/app/email-confirmations/email-confirmations.component.spec.ts
+++ angular/src/app/email-confirmations/email-confirmations.component.spec.ts
@@ -1,10 +1,10 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/Rx';
-import { CoreModule } from '../core/core.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+
+import { CoreModule } from '../core/core.module';
 
 import { SnackBarService } from '../core/snackService/snackService.service';
 import { BackendModule } from '../backend/backend.module';
@@ -22,22 +22,15 @@ describe('EmailConfirmationsComponent', () => {
         SnackBarService,
         EmailConfirmationsService,
         HttpClient,
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            url: Observable.of([{ path: 'booking' }, { path: 'cancel' }]),
-            params: Observable.of({ id: 123 }),
-          },
-        }],
+      ],
       imports: [
         CoreModule,
         BackendModule.forRoot({ environmentType: 0, restServiceRoot: 'v1' }),
         BrowserAnimationsModule,
         RouterTestingModule,
-        HttpClientModule,
       ],
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
