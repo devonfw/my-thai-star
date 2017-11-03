ed3c4a1f8c5365612d3013d3052668d441df2f94
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2017-11-03 13:56:09 +0100

    Small style and code fixes.

--- angular/src/app/email-confirmations/email-confirmations.component.spec.ts
+++ angular/src/app/email-confirmations/email-confirmations.component.spec.ts
@@ -2,10 +2,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
 
 import { CoreModule } from '../core/core.module';
-
 import { SnackBarService } from '../core/snackService/snackService.service';
 import { BackendModule } from '../backend/backend.module';
 import { EmailConfirmationsService } from './shared/email-confirmations.service';
