adfad9ff30cb19b10908e3e20855345a2a089015
Cristobal Belda <cbeldape@capgemini.com>
2017-12-11 13:08:35 +0100

    removed environments but prod and dev #25

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { BackendType } from '../app/config';
 export const environment: {production: boolean, backendType: BackendType, restPathRoot: string, restServiceRoot: string} = {
   production: true,
   backendType: BackendType.REST,
-  restPathRoot: 'http://de-mucdevondepl01:9090/mythaistar/',
-  restServiceRoot: 'http://de-mucdevondepl01:9090/mythaistar/services/rest/',
+  restPathRoot: 'http://localhost:8081/mythaistar/',
+  restServiceRoot: 'http://localhost:8081/mythaistar/services/rest/',
 };
