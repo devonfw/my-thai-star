87fe6fedaa37ad9a5c6c7d12a88b06821fd0a5cc
Garcia Marin <roberto.garcia-marin@capgemini.com>
2018-02-15 16:57:58 +0100

    forms updated to use FormGroups && reviewed falsy error fields when submited

--- angular/src/app/book-table/book-table.component.ts
+++ angular/src/app/book-table/book-table.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialog, MatDialogRef, MatCheckbox } from '@angular/material';
 import { BookTableDialogComponent } from './book-table-dialog/book-table-dialog.component';
 import { InvitationDialogComponent } from './invitation-dialog/invitation-dialog.component';
 import { WindowService } from '../core/windowService/windowService.service';
 import { SnackBarService } from '../core/snackService/snackService.service';
-import { emailValidator } from '../shared/directives/email-validator.directive';
+import { emailValidator, EmailValidatorDirective } from '../shared/directives/email-validator.directive';
 import { last } from 'lodash';
+import { ReservationInfo, BookingInfo } from 'app/shared/backendModels/interfaces';
+import { AbstractClassPart } from '@angular/compiler/src/output/output_ast';
+import { AbstractControl } from '@angular/forms/src/model';
 
 @Component({
   selector: 'public-book-table',
@@ -14,37 +17,82 @@ import { last } from 'lodash';
   styleUrls: ['./book-table.component.scss'],
 })
 
-export class BookTableComponent {
+export class BookTableComponent implements OnInit {
 
   invitationModel: string[] = [];
   minDate: Date = new Date();
+  bookForm: FormGroup;
+  invitationForm: FormGroup;
+
+  reservationInfo: BookingInfo = {
+    booking: {
+      name: '',
+      email: '',
+      bookingDate: undefined,
+      bookingType: 0,
+    },
+    invitedGuests: undefined,
+  };
 
   constructor(public window: WindowService,
               public snackBarservice: SnackBarService,
               public dialog: MatDialog) {
   }
 
-  showBookTableDialog(form: FormGroup): void {
-    let dialogRef: MatDialogRef<BookTableDialogComponent> = this.dialog.open(BookTableDialogComponent, {
-      width: this.window.responsiveWidth(),
-      data: form.value,
+  ngOnInit(): void {
+    this.invitationForm = new FormGroup({
+      'bookingDate': new FormControl(this.reservationInfo.booking.bookingDate, Validators.required),
+      'name': new FormControl(this.reservationInfo.booking.name, Validators.required),
+      'email': new FormControl(this.reservationInfo.booking.email, [
+        Validators.required,
+        Validators.pattern(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i),
+      ]),
+      'invitedGuests': new FormControl(this.invitationModel),
     });
-    dialogRef.afterClosed().subscribe((res: boolean) => {
+
+    this.bookForm = new FormGroup({
+      'bookingDate': new FormControl(this.reservationInfo.booking.bookingDate, Validators.required),
+      'name': new FormControl(this.reservationInfo.booking.name, Validators.required),
+      'email': new FormControl(this.reservationInfo.booking.email, [
+        Validators.required,
+        Validators.pattern(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i),
+      ]),
+      'assistants': new FormControl(this.reservationInfo.booking.assistants, [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(8),
+      ]),
+    });
+  }
+
+  get name(): AbstractControl { return this.bookForm.get('name'); }
+  get email(): AbstractControl { return this.bookForm.get('email'); }
+  get assistants(): AbstractControl { return this.bookForm.get('assistants'); }
+
+  get invName(): AbstractControl { return this.invitationForm.get('name'); }
+  get invEmail(): AbstractControl { return this.invitationForm.get('email'); }
+
+  showBookTableDialog(checkbox: MatCheckbox): void {
+    this.dialog.open(BookTableDialogComponent, {
+      width: this.window.responsiveWidth(),
+      data: this.bookForm.value,
+    }).afterClosed().subscribe((res: boolean) => {
       if (res) {
-        form.reset();
+        this.bookForm.reset();
+        checkbox.checked = false;
       }
     });
   }
 
-  showInviteDialog(form: FormGroup): void {
-    let dialogRef: MatDialogRef<InvitationDialogComponent> = this.dialog.open(InvitationDialogComponent, {
+  showInviteDialog(checkbox: MatCheckbox): void {
+    this.dialog.open(InvitationDialogComponent, {
       width: this.window.responsiveWidth(),
-      data: form.value,
-    });
-    dialogRef.afterClosed().subscribe((res: boolean) => {
+      data: this.invitationForm.value,
+    }).afterClosed().subscribe((res: boolean) => {
       if (res) {
-        form.reset();
+        this.invitationForm.reset();
         this.invitationModel = [];
+        checkbox.checked = false;
       }
     });
   }
