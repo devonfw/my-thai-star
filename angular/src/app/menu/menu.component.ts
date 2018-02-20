e6e73e8d603713ffafa0af2a4572895a61ced688
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2018-02-20 13:13:38 +0100

    Interface fixed. Some line endings of spec.ts files converted to LF.

--- angular/src/app/menu/menu.component.ts
+++ angular/src/app/menu/menu.component.ts
@@ -11,35 +11,34 @@ import { DishView } from '../shared/viewModels/interfaces';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+  menus: Observable<DishView[]>;
+  sortDir: string = 'DESC';
+  sortDirIcon: string = 'vertical_align_bottom';
 
-    menus: Observable<DishView[]>;
-    sortDir: string = 'DESC';
-    sortDirIcon: string = 'vertical_align_bottom';
+  constructor(private menuService: MenuService) {
+  }
 
-    constructor (private menuService: MenuService) {
-    }
+  ngOnInit(): void {
+    this.applyFilters();
+  }
 
-    ngOnInit(): void {
-      this.applyFilters();
-    }
+  changeSortDir(): void {
+    this.sortDir = (this.sortDir === 'ASC') ? 'DESC' : 'ASC';
+    this.sortDirIcon = (this.sortDirIcon === 'vertical_align_bottom') ? 'vertical_align_top' : 'vertical_align_bottom';
+  }
 
-    changeSortDir(): void {
-      this.sortDir = (this.sortDir === 'ASC') ? 'DESC' : 'ASC';
-      this.sortDirIcon = (this.sortDirIcon === 'vertical_align_bottom') ? 'vertical_align_top' : 'vertical_align_bottom';
-    }
+  applyFilters(filters?: any): void {
+    this.menus = this.menuService.getDishes(this.menuService.composeFilters(filters, this.sortDir));
+  }
 
-    applyFilters(filters?: any): void {
-      this.menus = this.menuService.getDishes(this.menuService.composeFilters(filters, this.sortDir));
-    }
+  clearFilters(form: FormGroup, price: MatSlider, likes: MatSlider): void {
+    likes.value = 0;
+    price.value = 0;
+    form.reset();
+    this.applyFilters();
+  }
 
-    clearFilters(form: FormGroup, price: MatSlider, likes: MatSlider): void {
-      likes.value = 0;
-      price.value = 0;
-      form.reset();
-      this.applyFilters();
-    }
-
-    clearSearchField(searchBy: MatInput): void {
-      searchBy.value = '';
-    }
+  clearSearchField(searchBy: MatInput): void {
+    searchBy.value = '';
+  }
 }
