59eba4e8b8f9ba594de1e9e8d4c8e874d6538744
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2018-02-27 16:06:16 +0100

    Typo fixed and CA language added.

--- angular/src/app/config.ts
+++ angular/src/app/config.ts
@@ -1,26 +1,25 @@
 export const config: any = {
-    pageSizes: [8, 16, 24],
-    pageSizesDialog: [4, 8, 12],
-    roles: [
-        { name: 'CUSTOMER', permission: 0 },
-        { name: 'WAITER', permission: 1 },
-    ],
-    langs: [
-        { label: 'English', value: 'en' },
-        { label: 'Español', value: 'es' },
-        { label: 'हिन्दी', value: 'hi' },
-        { label: 'Deutsch', value: 'de' },
-        { label: 'Français', value: 'fr' },
-        { label: 'Polski', value: 'pl' },
-        { label: 'Nederlands', value: 'nl' },
-        { label: 'български', value: 'bg' },
-        { label: 'Català', value: 'ca' },
-    ],
-    langs: [{ label: 'English', value: 'en' }, { label: 'Español', value: 'es' }],
+  pageSizes: [8, 16, 24],
+  pageSizesDialog: [4, 8, 12],
+  roles: [
+    { name: 'CUSTOMER', permission: 0 },
+    { name: 'WAITER', permission: 1 },
+  ],
+  langs: [
+    { label: 'English', value: 'en' },
+    { label: 'Español', value: 'es' },
+    { label: 'हिन्दी', value: 'hi' },
+    { label: 'Deutsch', value: 'de' },
+    { label: 'Français', value: 'fr' },
+    { label: 'Polski', value: 'pl' },
+    { label: 'Nederlands', value: 'nl' },
+    { label: 'български', value: 'bg' },
+    { label: 'Català', value: 'ca' },
+  ],
 };
 
 export enum BackendType {
-    IN_MEMORY,
-    REST,
-    GRAPHQL,
+  IN_MEMORY,
+  REST,
+  GRAPHQL,
 }
