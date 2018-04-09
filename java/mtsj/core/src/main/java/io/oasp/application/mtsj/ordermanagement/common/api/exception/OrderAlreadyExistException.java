56845ce758ca055f6b94bfc79625d15b5019a703
Sarmokadam <sneha.sarmokadam@capgemini.com>
2018-04-09 16:14:53 +0530

    changes for issue#82

--- java/mtsj/core/src/main/java/io/oasp/application/mtsj/ordermanagement/common/api/exception/OrderAlreadyExistException.java
+++ java/mtsj/core/src/main/java/io/oasp/application/mtsj/ordermanagement/common/api/exception/OrderAlreadyExistException.java
@@ -1,8 +1,6 @@
 package io.oasp.application.mtsj.ordermanagement.common.api.exception;
 
-import net.sf.mmm.util.exception.api.NlsRuntimeException;
-
-import javax.validation.ValidationException;
+import net.sf.mmm.util.exception.api.BusinessErrorUserException;
 
 import io.oasp.application.mtsj.bookingmanagement.dataaccess.api.BookingEntity;
 import io.oasp.application.mtsj.ordermanagement.dataaccess.api.OrderEntity;
@@ -11,9 +9,10 @@ import io.oasp.application.mtsj.ordermanagement.dataaccess.api.OrderEntity;
  * This exception is thrown if a {@link BookingEntity} has already {@link OrderEntity} related.
  *
  */
-public class OrderAlreadyExistException extends ValidationException {
+public class OrderAlreadyExistException extends BusinessErrorUserException {
 
   public OrderAlreadyExistException() {
-    super("The order for this booking already exist. Please cancel the order before create a new one.");
+
+    super(null, "The order for this booking already exist. Please cancel the order before create a new one.");
   }
 }
