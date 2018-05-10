fccb6fc2e51ab89835e4d5a253bb2e81053c93d2
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2018-05-10 09:12:45 +0200

    Angular 6 update (in progress)

--- angular/src/app/cockpit-area/shared/waiter-cockpit.service.ts
+++ angular/src/app/cockpit-area/shared/waiter-cockpit.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { PriceCalculatorService } from '../../sidenav/shared/price-calculator.service';
 import { BookingResponse, OrderResponse, OrderView } from '../../shared/viewModels/interfaces';
 import { map, cloneDeep } from 'lodash';
