0ec796c02907336d4c23d1f07236ab2f063cb904
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2018-05-10 16:17:02 +0200

    Angular 6 update:
    - md2 time and date picker removed
    - ng-pick-datetime added
    - material 6 fixes

--- angular/src/app/user-area/shared/user-area.service.ts
+++ angular/src/app/user-area/shared/user-area.service.ts
@@ -9,71 +9,100 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class UserAreaService {
+  private readonly loginRestPath: string = 'login';
+  private readonly currentUserRestPath: string = 'security/v1/currentuser/';
+  private readonly registerRestPath: string = 'register';
+  private readonly changePasswordRestPath: string = 'changepassword';
+  authAlerts: any;
 
-    private readonly loginRestPath: string = 'login';
-    private readonly currentUserRestPath: string = 'security/v1/currentuser/';
-    private readonly registerRestPath: string = 'register';
-    private readonly changePasswordRestPath: string = 'changepassword';
-    authAlerts: any;
+  constructor(
+    public snackBar: SnackBarService,
+    public router: Router,
+    public translate: TranslateService,
+    private http: HttpClient,
+    public authService: AuthService,
+  ) {
+    this.translate.get('alerts.authAlerts').subscribe((result: any) => {
+      this.authAlerts = result;
+    });
+  }
 
-    constructor(public snackBar: SnackBarService,
-        public router: Router,
-        public translate: TranslateService,
-        private http: HttpClient,
-        public authService: AuthService) {
-            this.translate.get('alerts.authAlerts')
-                .subscribe( (result: any) => {
-                    this.authAlerts = result;
-                });
-         }
-
-    login(username: string, password: string): void {
-        this.http.post(`${environment.restPathRoot}${this.loginRestPath}`,
-            { username: username, password: password }, { responseType: 'text', observe: 'response' })
-            .subscribe((res: any) => {
-                this.authService.setToken(res.headers.get('Authorization'));
-                this.http.get(`${environment.restServiceRoot}${this.currentUserRestPath}`)
-                    .subscribe((loginInfo: any) => {
-                        this.authService.setLogged(true);
-                        this.authService.setUser(loginInfo.name);
-                        this.authService.setRole(loginInfo.role);
-                        this.router.navigate(['orders']);
-                        this.snackBar.openSnack(this.authAlerts.loginSuccess, 4000, 'green');
-                    });
-            }, (err: any) => {
-                this.authService.setLogged(false);
-                this.snackBar.openSnack(err.message, 4000, 'red');
+  login(username: string, password: string): void {
+    this.http
+      .post(
+        `${environment.restPathRoot}${this.loginRestPath}`,
+        { username: username, password: password },
+        { responseType: 'text', observe: 'response' },
+      )
+      .subscribe(
+        (res: any) => {
+          this.authService.setToken(res.headers.get('Authorization'));
+          this.http
+            .get(`${environment.restServiceRoot}${this.currentUserRestPath}`)
+            .subscribe((loginInfo: any) => {
+              this.authService.setLogged(true);
+              this.authService.setUser(loginInfo.name);
+              this.authService.setRole(loginInfo.role);
+              this.router.navigate(['orders']);
+              this.snackBar.openSnack(
+                this.authAlerts.loginSuccess,
+                4000,
+                'green',
+              );
             });
-    }
+        },
+        (err: any) => {
+          this.authService.setLogged(false);
+          this.snackBar.openSnack(err.message, 4000, 'red');
+        },
+      );
+  }
 
-    register(email: string, password: string): void {
-        this.http.post(`${environment.restServiceRoot}${this.registerRestPath}`, {email: email, password: password})
-            .map((res: LoginInfo) => res)
-            .subscribe(() => {
-                this.snackBar.openSnack(this.authAlerts.registerSuccess, 4000, 'green');
-            }, (error: any) => {
-                this.snackBar.openSnack(this.authAlerts.registerFail, 4000, 'red');
-            });
-    }
+  register(email: string, password: string): void {
+    this.http
+      .post(`${environment.restServiceRoot}${this.registerRestPath}`, {
+        email: email,
+        password: password,
+      })
+      // .map((res: LoginInfo) => res)
+      .subscribe(
+        () => {
+          this.snackBar.openSnack(
+            this.authAlerts.registerSuccess,
+            4000,
+            'green',
+          );
+        },
+        (error: any) => {
+          this.snackBar.openSnack(this.authAlerts.registerFail, 4000, 'red');
+        },
+      );
+  }
 
-    logout(): void {
-        this.authService.setLogged(false);
-        this.authService.setUser('');
-        this.authService.setRole('CUSTOMER');
-        this.authService.setToken('');
-        this.router.navigate(['restarant']);
-        this.snackBar.openSnack(this.authAlerts.logoutSuccess, 4000, 'black');
-    }
+  logout(): void {
+    this.authService.setLogged(false);
+    this.authService.setUser('');
+    this.authService.setRole('CUSTOMER');
+    this.authService.setToken('');
+    this.router.navigate(['restarant']);
+    this.snackBar.openSnack(this.authAlerts.logoutSuccess, 4000, 'black');
+  }
 
-    changePassword(data: any): void {
-        data.username = this.authService.getUser();
-        this.http.post(`${environment.restServiceRoot}${this.changePasswordRestPath}`,
-        { username: data.username, oldPassword: data.oldPassword, newPassword: data.newPassword })
-            .subscribe((res: any) => {
-                this.snackBar.openSnack(res.message, 4000, 'green');
-            }, (error: any) => {
-                this.snackBar.openSnack(error.message, 4000, 'red');
-            });
-    }
-
+  changePassword(data: any): void {
+    data.username = this.authService.getUser();
+    this.http
+      .post(`${environment.restServiceRoot}${this.changePasswordRestPath}`, {
+        username: data.username,
+        oldPassword: data.oldPassword,
+        newPassword: data.newPassword,
+      })
+      .subscribe(
+        (res: any) => {
+          this.snackBar.openSnack(res.message, 4000, 'green');
+        },
+        (error: any) => {
+          this.snackBar.openSnack(error.message, 4000, 'red');
+        },
+      );
+  }
 }
