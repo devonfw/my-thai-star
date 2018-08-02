931637231e724cb3493d630a7d83c9d7ef919539
Bartlomiej Pietrowski <bartlomiej.pietrowski@capgemini.com>
2018-08-02 17:47:27 +0200

    Improve home component - split into presentational components, extract resolutions to scss variable, allow "own" as prefix for internal component prefixes

---
+++ angular/src/app/home/home-layout/home-layout.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeLayoutComponent } from './home-layout.component';
+
+describe('HomeLayoutComponent', () => {
+  let component: HomeLayoutComponent;
+  let fixture: ComponentFixture<HomeLayoutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeLayoutComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
