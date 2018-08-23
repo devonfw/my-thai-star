d096abebb10a7c7f36349c332fd587247a156656
Bartlomiej Pietrowski <bartlomiej.pietrowski@capgemini.com>
2018-08-23 15:43:09 +0200

    Move review comments to a a separate component.

---
+++ angular/src/app/menu/menu-card/menu-card-comments/menu-card-comments.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenuCardCommentsComponent } from './menu-card-comments.component';
+
+describe('MenuCardCommentsComponent', () => {
+  let component: MenuCardCommentsComponent;
+  let fixture: ComponentFixture<MenuCardCommentsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MenuCardCommentsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MenuCardCommentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
