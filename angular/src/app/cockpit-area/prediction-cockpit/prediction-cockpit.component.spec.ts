5a04217255f53aabceed277409a17cc93a82cfd9
CORP\vapadwal <vaibhav.padwal@capgemini.com>
2019-04-20 19:26:33 +0530

    SAP Hana Integration

--- angular/src/app/cockpit-area/prediction-cockpit/prediction-cockpit.component.spec.ts
+++ angular/src/app/cockpit-area/prediction-cockpit/prediction-cockpit.component.spec.ts
@@ -1,36 +1,55 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CoreModule } from '../../core/core.module';
+import { HttpClient/*, HttpClientModule*/ } from '@angular/common/http';
+//import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { CoreModule } from '../../core/core.module';
 import { PredictionCockpitComponent } from './prediction-cockpit.component';
 import { PredictionService } from '../shared/prediction.service';
-import { ReactiveFormsModule } from '@angular/forms';
+//import { ReactiveFormsModule } from '@angular/forms';
+import { PriceCalculatorService } from '../../sidenav/shared/price-calculator.service';
+import { ConfigService } from '../../core/config/config.service';
+import { MatDialog } from '@angular/material';
+import { TranslateService } from '@ngx-translate/core';
 
 describe('PredictionCockpitComponent', () => {
   let component: PredictionCockpitComponent;
-  let fixture: ComponentFixture<PredictionCockpitComponent>;
+  //let fixture: ComponentFixture<PredictionCockpitComponent>;
+  let http: HttpClient;
+  let configService: ConfigService;
+  let predictionService: PredictionService;
+  // tslint:disable-next-line:prefer-const
+  let translate: TranslateService;
+  // tslint:disable-next-line:prefer-const
+  let dialog: MatDialog;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [PredictionCockpitComponent],
-      providers: [
-        HttpClient, 
-        PredictionService,
-      ],
-      imports: [
-        CoreModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-		ReactiveFormsModule,
-      ],
-    })
-    .compileComponents();
-  }));
+  //beforeEach(async(() => {
+  //  TestBed.configureTestingModule({
+   //   declarations: [PredictionCockpitComponent],
+    //  providers: [
+     //   HttpClient, 
+       // PredictionService,
+  //    ],
+  //    imports: [
+  //      CoreModule,
+  //      BrowserAnimationsModule,
+  //      HttpClientModule,
+	//	ReactiveFormsModule,
+   //   ],
+   // })
+   // .compileComponents();
+  // }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PredictionCockpitComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture = TestBed.createComponent(PredictionCockpitComponent);
+    //component = fixture.componentInstance;
+    //fixture.detectChanges();
+    configService = new ConfigService(http);
+    predictionService = new PredictionService(
+      http,
+      configService,
+    );
+    component = new PredictionCockpitComponent(
+      predictionService,
+    );
   });
 
   it('should create', () => {
