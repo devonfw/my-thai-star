d6266e52d832f20bb30d8db3597e39972b99836b
Rohitkumar <rai.rohitkumar@capgemini.com>
2019-04-25 18:02:21 +0530

    Refactored SMTP details issue204

--- java/mtsj/core/src/main/java/com/devonfw/application/mtsj/mailservice/logic/impl/MailMock.java
+++ java/mtsj/core/src/main/java/com/devonfw/application/mtsj/mailservice/logic/impl/MailMock.java
@@ -4,7 +4,6 @@ import javax.inject.Named;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 import com.devonfw.application.mtsj.mailservice.logic.api.Mail;
@@ -17,8 +16,9 @@ import com.devonfw.application.mtsj.mailservice.logic.api.Mail;
 @ConditionalOnProperty(prefix = "spring.mail", name = "enabled", havingValue = "false")
 public class MailMock implements Mail {
 
-  @Value("${spring.mail.username}")
-  private String from;
+  /*
+   * @Value("${spring.mail.username}") private String from;
+   */
 
   /**
    * Logger instance.
@@ -29,7 +29,7 @@ public class MailMock implements Mail {
   public boolean sendMail(String to, String subject, String text) {
 
     StringBuilder sb = new StringBuilder();
-    sb.append("From: ").append(this.from).append("|").append("To: ").append(to).append("|").append("Subject: ")
+    sb.append("From: ")/* .append(this.from) */.append("|").append("To: ").append(to).append("|").append("Subject: ")
         .append(subject).append("|").append("Text: ").append(text);
     LOG.info(sb.toString());
     return true;
