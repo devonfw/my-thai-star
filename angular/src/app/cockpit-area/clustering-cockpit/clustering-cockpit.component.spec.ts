79515a70ad48009d666b33e119a7f0ac9075f04e
Jonathan Bregler <jonathan.bregler@sap.com>
2019-05-08 12:26:58 +0200

    Add spatial clustering use case for SAP HANA
    - Add clustering cockpit and service

---
+++ angular/src/app/cockpit-area/clustering-cockpit/clustering-cockpit.component.spec.ts
@@ -0,0 +1,38 @@
+import { HttpClient/*, HttpClientModule*/ } from '@angular/common/http';
+import { ClusteringCockpitComponent } from './clustering-cockpit.component';
+import { ClusteringService } from '../shared/clustering.service';
+import { PriceCalculatorService } from '../../sidenav/shared/price-calculator.service';
+import { ConfigService } from '../../core/config/config.service';
+import { MatDialog } from '@angular/material';
+import { TranslateService } from '@ngx-translate/core';
+import { MenuService } from '../../menu/shared/menu.service';
+
+describe('ClusteringCockpitComponent', () => {
+  let component: ClusteringCockpitComponent;
+  let http: HttpClient;
+  let configService: ConfigService;
+  let clusteringService: ClusteringService;
+  let menuService: MenuService;
+  let translate: TranslateService;
+  let dialog: MatDialog;
+
+  beforeEach(() => {
+    configService = new ConfigService(http);
+    clusteringService = new ClusteringService(
+      http,
+      configService,
+    );
+    menuService = new MenuService(
+      http,
+      configService,
+    );
+    component = new ClusteringCockpitComponent(
+      clusteringService,
+      menuService,
+    );
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
