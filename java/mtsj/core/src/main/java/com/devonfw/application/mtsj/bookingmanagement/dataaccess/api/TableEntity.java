c9958fde42804ac0cdf790fff28a41f62602c40c
TL <49901332+tliszyns@users.noreply.github.com>
2019-05-27 11:38:06 +0200

    issue #188: change name of table Table to DinnerTable

--- java/mtsj/core/src/main/java/com/devonfw/application/mtsj/bookingmanagement/dataaccess/api/TableEntity.java
+++ java/mtsj/core/src/main/java/com/devonfw/application/mtsj/bookingmanagement/dataaccess/api/TableEntity.java
@@ -6,7 +6,7 @@ import com.devonfw.application.mtsj.bookingmanagement.common.api.Table;
 import com.devonfw.application.mtsj.general.dataaccess.api.ApplicationPersistenceEntity;
 
 @Entity
-@javax.persistence.Table(name = "Table")
+@javax.persistence.Table(name = "DinnerTable")
 public class TableEntity extends ApplicationPersistenceEntity implements Table {
 
   private Integer seatsNumber;
