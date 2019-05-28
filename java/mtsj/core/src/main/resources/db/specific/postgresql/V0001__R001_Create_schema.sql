1c925b2cc7b471fac8863199069212d804e51d3e
tliszyns <49901332+tliszyns@users.noreply.github.com>
2019-05-28 09:25:42 +0200

    Revert "Issue188 postgres schema"

--- java/mtsj/core/src/main/resources/db/specific/postgresql/V0001__R001_Create_schema.sql
+++ java/mtsj/core/src/main/resources/db/specific/postgresql/V0001__R001_Create_schema.sql
@@ -7,8 +7,8 @@
 CREATE SEQUENCE HIBERNATE_SEQUENCE START WITH 1000000;
 
 -- *** Table ***
-CREATE TABLE "TABLE" (
-  id BIGSERIAL NOT NULL,
+CREATE TABLE Table (
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   seatsNumber INTEGER NOT NULL,
   CONSTRAINT PK_Table PRIMARY KEY(id)
@@ -16,7 +16,7 @@ CREATE TABLE "TABLE" (
 
 -- *** UserRole ***
 CREATE TABLE UserRole (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   name VARCHAR (255),
   active BOOLEAN,
@@ -24,20 +24,20 @@ CREATE TABLE UserRole (
 );
 
 -- *** User ***
-CREATE TABLE USER (
-  id BIGSERIAL NOT NULL,
+CREATE TABLE User (
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   username VARCHAR (255) NULL,
   password VARCHAR (255) NULL,
   email VARCHAR (120) NULL,
   idRole BIGINT NOT NULL,
   CONSTRAINT PK_User PRIMARY KEY(id),
-  CONSTRAINT PK_User_idRole FOREIGN KEY(idRole) REFERENCES UserRole(id)
+  CONSTRAINT PK_User_idRole FOREIGN KEY(idRole) REFERENCES UserRole(id) NOCHECK
 );
 
 -- *** Booking ***
 CREATE TABLE Booking (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   idUser BIGINT,
   name VARCHAR (255) NOT NULL,
@@ -47,19 +47,19 @@ CREATE TABLE Booking (
   bookingDate TIMESTAMP NOT NULL,
   expirationDate TIMESTAMP,
   creationDate TIMESTAMP,
-  canceled BOOLEAN NOT NULL DEFAULT ((FALSE)),
+  canceled BOOLEAN NOT NULL DEFAULT ((0)) ,
   bookingType INTEGER,
   idTable BIGINT,
   idOrder BIGINT,
   assistants INTEGER,
   CONSTRAINT PK_Booking PRIMARY KEY(id),
-  CONSTRAINT FK_Booking_idUser FOREIGN KEY(idUser) REFERENCES USER(id),
-  CONSTRAINT FK_Booking_idTable FOREIGN KEY(idTable) REFERENCES "TABLE"(id)
+  CONSTRAINT FK_Booking_idUser FOREIGN KEY(idUser) REFERENCES User(id) NOCHECK,
+  CONSTRAINT FK_Booking_idTable FOREIGN KEY(idTable) REFERENCES Table(id) NOCHECK
 );
 
 -- *** InvitedGuest ***
 CREATE TABLE InvitedGuest (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   idBooking BIGINT NOT NULL,
   guestToken VARCHAR (60),
@@ -68,37 +68,37 @@ CREATE TABLE InvitedGuest (
   modificationDate TIMESTAMP,
   idOrder BIGINT,
   CONSTRAINT PK_InvitedGuest PRIMARY KEY(id),
-  CONSTRAINT FK_InvitedGuest_idBooking FOREIGN KEY(idBooking) REFERENCES Booking(id)
+  CONSTRAINT FK_InvitedGuest_idBooking FOREIGN KEY(idBooking) REFERENCES Booking(id) NOCHECK
 );
 
 -- *** OrderDish ***
 CREATE TABLE Orders (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   idBooking BIGINT NOT NULL,
   idInvitedGuest BIGINT,
   idHost BIGINT,
   CONSTRAINT PK_Order PRIMARY KEY(id),
-  CONSTRAINT FK_Order_idBooking FOREIGN KEY(idBooking) REFERENCES Booking(id),
-  CONSTRAINT FK_Order_idInvitedGuest FOREIGN KEY(idInvitedGuest) REFERENCES InvitedGuest(id)
+  CONSTRAINT FK_Order_idBooking FOREIGN KEY(idBooking) REFERENCES Booking(id) NOCHECK,
+  CONSTRAINT FK_Order_idInvitedGuest FOREIGN KEY(idInvitedGuest) REFERENCES InvitedGuest(id) NOCHECK
 );
 
 -- *** Category ***
 CREATE TABLE Category (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   name VARCHAR (255),
   description VARCHAR (4000),
   showOrder INTEGER,
-  CONSTRAINT PK_Category PRIMARY KEY(id)
+  CONSTRAINT PK_Category PRIMARY KEY(id),
 );
 
 -- *** Image ***
 CREATE TABLE Image (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   name VARCHAR(255),
-  content text,
+  content VARCHAR(2147483647),
   contentType INTEGER,
   mimeType VARCHAR(255),
   CONSTRAINT PK_Image PRIMARY KEY(id)
@@ -106,30 +106,30 @@ CREATE TABLE Image (
 
 -- *** Dish ***
 CREATE TABLE Dish (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   name VARCHAR (255),
   description VARCHAR (4000),
   price DECIMAL (16,10),
   idImage BIGINT UNIQUE NOT NULL,
   CONSTRAINT PK_Dish PRIMARY KEY(id),
-  CONSTRAINT FK_Dish_idImage FOREIGN KEY(idImage) REFERENCES Image(id)
+  CONSTRAINT FK_Dish_idImage FOREIGN KEY(idImage) REFERENCES Image(id) NOCHECK,
 );
 
 -- *** DishCategory ***
 CREATE TABLE DishCategory (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   idDish BIGINT NOT NULL,
   idCategory BIGINT NOT NULL,
   CONSTRAINT PK_DishCategory PRIMARY KEY(id),
-  CONSTRAINT FK_DishCategory_idDish FOREIGN KEY(idDish) REFERENCES Dish(id),
-  CONSTRAINT FK_DishCategory_idCategory FOREIGN KEY(idCategory) REFERENCES Category(id)
+  CONSTRAINT FK_DishCategory_idDish FOREIGN KEY(idDish) REFERENCES Dish(id) NOCHECK,
+  CONSTRAINT FK_DishCategory_idCategory FOREIGN KEY(idCategory) REFERENCES Category(id) NOCHECK
 );
 
 -- *** Ingredient ***
 CREATE TABLE Ingredient (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   name VARCHAR (255),
   description VARCHAR (4000),
@@ -139,57 +139,57 @@ CREATE TABLE Ingredient (
 
 -- *** DishIngredient ***
 CREATE TABLE DishIngredient (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   idDish BIGINT NOT NULL,
   idIngredient BIGINT NOT NULL,
   CONSTRAINT PK_DishIngredient PRIMARY KEY(id),
-  CONSTRAINT FK_DishIngredient_idDish FOREIGN KEY(idDish) REFERENCES Dish(id),
-  CONSTRAINT FK_DishIngredient_idIngredient FOREIGN KEY(idIngredient) REFERENCES Ingredient(id)
+  CONSTRAINT FK_DishIngredient_idDish FOREIGN KEY(idDish) REFERENCES Dish(id) NOCHECK,
+  CONSTRAINT FK_DishIngredient_idIngredient FOREIGN KEY(idIngredient) REFERENCES Ingredient(id) NOCHECK
 );
 
 -- *** OrderLine ***
 CREATE TABLE OrderLine (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   idDish BIGINT NOT NULL,
   amount INTEGER,
   comment VARCHAR (255),
   idOrder BIGINT NOT NULL,
   CONSTRAINT PK_OrderLine PRIMARY KEY(id),
-  CONSTRAINT FK_OrderLine_idDish FOREIGN KEY(idDish) REFERENCES Dish(id),
-  CONSTRAINT FK_OrderLine_idOrder FOREIGN KEY(idOrder) REFERENCES Orders(id)
+  CONSTRAINT FK_OrderLine_idDish FOREIGN KEY(idDish) REFERENCES Dish(id) NOCHECK,
+  CONSTRAINT FK_OrderLine_idOrder FOREIGN KEY(idOrder) REFERENCES Orders(id) NOCHECK
 );
 
 -- *** OrderDishExtraIngredient ***
 CREATE TABLE OrderDishExtraIngredient (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER,
   idOrderLine BIGINT NOT NULL,
   idIngredient BIGINT NOT NULL,
   CONSTRAINT PK_OrderDishExtraIngredient PRIMARY KEY(id),
-  CONSTRAINT FK_OrderDishExtraIngredient_idOrderLine FOREIGN KEY(idOrderLine) REFERENCES OrderLine(id),
-  CONSTRAINT FK_OrderDishExtraIngredient_idIngredient FOREIGN KEY(idIngredient) REFERENCES Ingredient(id)
+  CONSTRAINT FK_OrderDishExtraIngredient_idOrderLine FOREIGN KEY(idOrderLine) REFERENCES OrderLine(id) NOCHECK,
+  CONSTRAINT FK_OrderDishExtraIngredient_idIngredient FOREIGN KEY(idIngredient) REFERENCES Ingredient(id) NOCHECK
 );
 
 -- *** UserFavourite ***
 CREATE TABLE UserFavourite (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
   idUser BIGINT NOT NULL,
   idDish BIGINT NOT NULL,
   CONSTRAINT PK_UserFavourite PRIMARY KEY(id),
-  CONSTRAINT FK_UserFavourite_idUser FOREIGN KEY(idUser) REFERENCES USER(id),
-  CONSTRAINT FK_UserFavourite_idDish FOREIGN KEY(idDish) REFERENCES Dish(id)
+  CONSTRAINT FK_UserFavourite_idUser FOREIGN KEY(idUser) REFERENCES User(id) NOCHECK,
+  CONSTRAINT FK_UserFavourite_idDish FOREIGN KEY(idDish) REFERENCES Dish(id) NOCHECK
 );
 
 -- *************************************************************************
 
 -- *** BinaryObject (BLOBs) ***
 CREATE TABLE BinaryObject (
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL AUTO_INCREMENT,
   modificationCounter INTEGER NOT NULL,
-  data OID,
+  data BLOB(2147483647),
   size BIGINT NOT NULL,
   mimeType VARCHAR(255),
   PRIMARY KEY (ID)
@@ -197,7 +197,7 @@ CREATE TABLE BinaryObject (
 
 -- *** RevInfo (Commit log for envers audit trail) ***
 CREATE TABLE RevInfo(
-  id BIGSERIAL NOT NULL,
+  id BIGINT NOT NULL GENERATED BY DEFAULT AS IDENTITY (START WITH 1),
   timestamp BIGINT NOT NULL,
   userLogin VARCHAR(255)
 );
