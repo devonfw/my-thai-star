c8b8d8b86f3505dadc430437761105e417f781dd
Malte Vu <malte.vu@capgemini.com>
2019-06-01 18:38:37 +0200

    Functional frontend with 2FA User can now obtain the QR-Code

--- angular/src/app/user-area/qr-code-dialog/qr-code-dialog.component.ts
+++ angular/src/app/user-area/qr-code-dialog/qr-code-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from "@angular/material";
 import { UserAreaService } from "../shared/user-area.service";
 import { SnackBarService } from "../../core/snack-bar/snack-bar.service";
@@ -10,7 +10,7 @@ import { SnackBarService } from "../../core/snack-bar/snack-bar.service";
 })
 export class QrCodeDialogComponent implements OnInit {
 
-  private qrcode: any;
+  private qrcode: string;
 
   constructor(
     private dialog: MatDialogRef<QrCodeDialogComponent>,
@@ -19,14 +19,13 @@ export class QrCodeDialogComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-
+    this.loadQrCode();
   }
 
   private loadQrCode(): void {
     this.userAreaService.pairing().subscribe(
       (res: any) => {
-        console.log(res);
-        
+        this.qrcode = res.body;
       },
       (err: any) => {
         this.snackBar.openSnack(err.message, 4000, 'red');
