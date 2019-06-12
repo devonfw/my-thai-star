7725d75a87d3c672925b53cbd99865e488058a67
Malte Vu <malte.vu@capgemini.com>
2019-06-12 20:55:55 +0200

    Fix v2

--- angular/src/app/user-area/shared/snack-bar.service.ts
+++ angular/src/app/user-area/shared/snack-bar.service.ts
@@ -1,5 +1,5 @@
-import {Injectable} from '@angular/core';
-import {SnackBarService} from "../../core/snack-bar/snack-bar.service";
+import { Injectable } from '@angular/core';
+import { SnackBarService } from '../../core/snack-bar/snack-bar.service';
 
 @Injectable({
   providedIn: 'root'
