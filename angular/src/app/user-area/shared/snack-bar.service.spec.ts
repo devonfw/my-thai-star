f494baa70237ce870ff30518d489a2c22defcb75
Malte Vu <malte.vu@capgemini.com>
2019-06-12 20:55:55 +0200

    Fix v2

--- angular/src/app/user-area/shared/snack-bar.service.spec.ts
+++ angular/src/app/user-area/shared/snack-bar.service.spec.ts
@@ -1,21 +1,20 @@
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed, inject } from '@angular/core/testing';
 
-import { SnackBarService } from '../../core/snack-bar/snack-bar.service';
-import { UserAreaService } from "./user-area.service";
+import { SnackService } from '../../user-area/shared/snack-bar.service';
+import { UserAreaService } from './user-area.service';
 import { AuthService } from '../../core/authentication/auth.service';
 import { CoreModule } from '../../core/core.module';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
 
-
-describe('UserAreaService', () => {
+describe('SnackService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         UserAreaService,
         AuthService,
-        SnackBarService,
+        SnackService,
       ],
       imports: [
         CoreModule,
@@ -26,7 +25,7 @@ describe('UserAreaService', () => {
     });
   });
 
-  it('should create', inject([UserAreaService], (service: UserAreaService) => {
+  it('should create', inject([SnackService], (service: SnackService) => {
     expect(service).toBeTruthy();
   }));
 });
