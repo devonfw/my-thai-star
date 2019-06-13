9a2fb2e1e08b015062e9669a5672a97c2677acb6
Malte Vu <malte.vu@capgemini.com>
2019-06-13 18:37:56 +0200

    Added remaining translation Remove translation service Make toogle available only when 2FA is activated

--- angular/src/app/user-area/qr-code-dialog/qr-code-dialog.component.ts
+++ angular/src/app/user-area/qr-code-dialog/qr-code-dialog.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { UserAreaService } from '../shared/user-area.service';
 import { SnackService } from '../shared/snack-bar.service';
 import { AuthService } from '../../core/authentication/auth.service';
-import { TranslateService } from '@ngx-translate/core';
 import { TwoFactorResponse } from '../../shared/view-models/interfaces';
 
 @Component({
@@ -15,28 +14,12 @@ export class QrCodeDialogComponent implements OnInit {
   public qrSecret = true;
   public qrcode: string;
   public secret: string;
-  public qrSecretText = 'QR';
-  private qrText = 'QR';
-  private secretText = 'Secret code';
 
   constructor(
     public authService: AuthService,
     private snackBar: SnackService,
     public userAreaService: UserAreaService,
-    public translate: TranslateService,
-  ) {
-    this.translate
-      .get('userArea.qrcodeDialog.qrSwitchText')
-      .subscribe((content: string = 'QR') => {
-        this.qrText = content;
-      });
-
-    this.translate
-      .get('userArea.qrcodeDialog.secretSwitchText')
-      .subscribe((content: string = 'Secret code') => {
-        this.secretText = content;
-      });
-  }
+  ) {}
 
   ngOnInit(): void {
     this.loadData();
@@ -63,12 +46,6 @@ export class QrCodeDialogComponent implements OnInit {
 
   public changeQrSecret(): void {
     this.qrSecret = !this.qrSecret;
-
-    if (this.qrSecret) {
-      this.qrSecretText = this.qrText;
-    } else {
-      this.qrSecretText = this.secretText;
-    }
   }
 
   public changeStatus(): void {
