4a961311b83be23c75ec9ec4edc19f68194d1b4e
Naser Kazimi <kaziminaser@hotmail.com>
2019-06-13 19:38:01 +0200

    tests implemented

--- angular/src/app/menu/container/menu.component.spec.ts
+++ angular/src/app/menu/container/menu.component.spec.ts
@@ -1,79 +1,69 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { /* ComponentFixture, */ TestBed, async } from '@angular/core/testing';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
-import { CoreModule } from '../../core/core.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { SidenavService } from '../../sidenav/services/sidenav.service';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ExtraView } from 'app/shared/view-models/interfaces';
+import { Store, Action } from '@ngrx/store';
 import { MenuService } from '../services/menu.service';
-import { SnackBarService } from '../../core/snack-bar/snack-bar.service';
-import { AuthService } from '../../core/authentication/auth.service';
-import { ConfigService } from '../../core/config/config.service';
-import { MatSlider, MatInput } from '@angular/material';
-
+import { SidenavService } from '../../sidenav/services/sidenav.service';
+import { FilterFormData } from '../components/menu-filters/menu-filters.component';
 import { MenuComponent } from './menu.component';
-import { MenuCardComponent } from '../components/menu-card/menu-card.component';
-import { MenuFiltersComponent } from '../components/menu-filters/menu-filters.component';
-import { FilterSearchComponent } from '../components/menu-filters/filter-search/filter-search.component';
-import { FilterCheckboxesComponent } from '../components/menu-filters/filter-checkboxes/filter-checkboxes.component';
-import { FilterSortComponent } from '../components/menu-filters/filter-sort/filter-sort.component';
-import { MenuCardCommentsComponent } from '../components/menu-card/menu-card-comments/menu-card-comments.component';
-import { MenuCardDetailsComponent } from '../components/menu-card/menu-card-details/menu-card-details.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { Subject } from 'rxjs';
+import * as fromStore from 'app/store/reducers';
+
+export function mockStore<T>({
+  actions = new Subject<Action>(),
+  states = new Subject<T>(),
+}: {
+  actions?: Subject<Action>;
+  states?: Subject<T>;
+}): Store<T> {
+  const result = states as any;
+  result.dispatch = (action: Action) => actions.next(action);
+  return result;
+}
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
-  // let fixture: ComponentFixture<MenuComponent>;
-  // tslint:disable-next-line:prefer-const
-  let http: HttpClient;
-  let configService: ConfigService;
-  let menuService: MenuService;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        MenuComponent,
-        MenuCardComponent,
-        MenuFiltersComponent,
-        MenuCardCommentsComponent,
-        MenuCardDetailsComponent,
-        FilterSearchComponent,
-        FilterCheckboxesComponent,
-        FilterSortComponent,
-      ],
-      providers: [
-        HttpClient,
-        SidenavService,
-        MenuService,
-        SnackBarService,
-        AuthService,
-        MatSlider,
-        MatInput,
-      ],
-      imports: [
-        BrowserAnimationsModule,
-        BrowserDynamicTestingModule,
-        HttpClientModule,
-        TranslateModule.forRoot(),
-        ReactiveFormsModule,
-        FormsModule,
-        CoreModule,
-      ],
-    }).compileComponents();
-  }));
+  let fixture: ComponentFixture<MenuComponent>;
+  const actions = new Subject<Action>();
+  const states = new Subject<fromStore.State>();
+  const store = mockStore<fromStore.State>({ actions, states });
 
   beforeEach(() => {
-    // fixture = TestBed.createComponent(MenuComponent);
-    // component = fixture.componentInstance;
-    // fixture.detectChanges();
-    configService = new ConfigService(http);
-    menuService = new MenuService(http, configService);
-    component = new MenuComponent(menuService);
+    const extraViewStub = {};
+    const storeStub = {
+      select: (arg1) => ({ subscribe: () => ({}) }),
+      dispatch: (arg1) => ({}),
+    };
+    const menuServiceStub = {
+      composeFilters: (pageable1, filters2) => ({}),
+      menuToOrder: (order1) => ({ dish: { id: {} } }),
+    };
+    const sidenavServiceStub = { openSideNav: () => ({}) };
+    const filterFormDataStub = { sort: { property: {}, direction: {} } };
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [MenuComponent],
+      providers: [
+        { provide: Store, useValue: storeStub },
+        { provide: MenuService, useValue: menuServiceStub },
+        { provide: SidenavService, useValue: sidenavServiceStub },
+        { provide: FilterFormData, useValue: filterFormDataStub },
+      ],
+    });
+    fixture = TestBed.createComponent(MenuComponent);
+    component = fixture.componentInstance;
   });
-
-  it('should create', () => {
+  it('can load instance', () => {
     expect(component).toBeTruthy();
+  });
+  it('extras defaults to: []', () => {
+    expect(component.extras).toEqual([]);
+  });
+  describe('ngOnInit', () => {
+    it('makes expected calls', () => {
+      spyOn(store, 'select').and.callThrough();
+      component.ngOnInit();
+      expect(store.select).toHaveBeenCalled();
+    });
   });
 });
--- angular/src/app/menu/store/effects/menu.effects.ts
+++ angular/src/app/menu/store/effects/menu.effects.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-
 import {catchError, map, mergeMap} from 'rxjs/operators';
 import {of} from 'rxjs';
 import {MenuActionTypes, MenuActions, LoadMenuFail, LoadMenusSuccess} from '../actions/menu.actions';
 import {MenuService} from '../../services/menu.service';
-
 
 @Injectable()
 export class MenuEffects {
