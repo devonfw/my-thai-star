651883be2eabd4017dca9e54c1cc34fea501904b
Naser Kazimi <naser.kazimi@capgemini.com>
2019-06-14 12:17:03 +0200

    Fix bugs

--- angular/src/app/cockpit-area/prediction-cockpit/prediction-cockpit.component.ts
+++ angular/src/app/cockpit-area/prediction-cockpit/prediction-cockpit.component.ts
@@ -4,11 +4,7 @@ import { FilterOrdersCockpit, PredictionCriteria } from '../../shared/backend-mo
 import { Chart } from 'chart.js';
 import { Moment } from 'moment';
 import { OwlDateTimeComponent } from 'ng-pick-datetime';
-<<<<<<< HEAD
 import { PredictionService } from '../services/prediction.service';
-=======
-import { PredictionService } from '../shared/prediction.service';
->>>>>>> upstream/develop
 
 @Component({
   selector: 'cockpit-prediction-cockpit',
