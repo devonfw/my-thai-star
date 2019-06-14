2c052e73624c0d09903ccb5c4199e0c575d2e15c
Naser Ka <47857806+NaserKa@users.noreply.github.com>
2019-06-14 14:28:20 +0200

    Update config.service.ts

--- angular/src/app/core/config/config.service.ts
+++ angular/src/app/core/config/config.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Config, config } from './config';
@@ -40,46 +39,3 @@ export class ConfigService {
     return config;
   }
 }
-=======
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Config, config } from './config';
-import { environment } from '../../../environments/environment';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class ConfigService {
-  constructor(private httpClient: HttpClient) {}
-
-  static factory(appLoadService: ConfigService) {
-    return () => appLoadService.loadExternalConfig();
-  }
-
-  // this method gets external configuration calling /config endpoint and merges into config object
-  loadExternalConfig(): Promise<any> {
-    if (!environment.loadExternalConfig) {
-      return Promise.resolve({});
-    }
-
-    const promise = this.httpClient
-      .get('/config')
-      .toPromise()
-      .then((settings) => {
-        Object.keys(settings || {}).forEach((k) => {
-          config[k] = settings[k];
-        });
-        return settings;
-      })
-      .catch(() => {
-        return 'ok, no external configuration';
-      });
-
-    return promise;
-  }
-
-  getValues(): Config {
-    return config;
-  }
-}
->>>>>>> upstream/develop
