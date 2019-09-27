27e44982afdf058aa1fc93d83698068fc44a1d5a
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2019-09-27 13:56:49 +0200

    Added type action for orders and send orders

--- angular/src/app/sidenav/store/actions/send-order.actions.ts
+++ angular/src/app/sidenav/store/actions/send-order.actions.ts
@@ -1,7 +1,23 @@
-import { props, createAction } from '@ngrx/store';
+import { props, createAction, union } from '@ngrx/store';
 
-export const sendOrders = createAction('[SendOrder] Load SendOrders', props<{token: string}>());
+export const sendOrders = createAction(
+  '[SendOrder] Load SendOrders',
+  props<{ token: string }>(),
+);
 
-export const sendOrdersSuccess = createAction('[SendOrder] Load SendOrders Success');
+export const sendOrdersSuccess = createAction(
+  '[SendOrder] Load SendOrders Success',
+);
 
-export const sendOrdersFail = createAction('[SendOrder] Load SendOrders Fail', props<{error: string}>());
+export const sendOrdersFail = createAction(
+  '[SendOrder] Load SendOrders Fail',
+  props<{ error: string }>(),
+);
+
+// action types
+const all = union({
+  sendOrders,
+  sendOrdersSuccess,
+  sendOrdersFail,
+});
+export type SendOrdersAction = typeof all;
