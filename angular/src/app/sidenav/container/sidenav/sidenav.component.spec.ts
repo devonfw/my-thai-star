62000461a05afe565158acf147f5f8b84594c364
Santos Jiménez Linares <sjimenez77@gmail.com>
2019-09-29 18:57:00 +0200

    Tests fixed and more refactoring

--- angular/src/app/sidenav/container/sidenav/sidenav.component.spec.ts
+++ angular/src/app/sidenav/container/sidenav/sidenav.component.spec.ts
@@ -2,45 +2,60 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { StoreModule } from '@ngrx/store';
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { PriceCalculatorService } from 'app/sidenav/services/price-calculator.service';
 import { SidenavService } from 'app/sidenav/services/sidenav.service';
 import { CoreModule } from '../../../core/core.module';
 import { SnackBarService } from '../../../core/snack-bar/snack-bar.service';
-import * as fromStore from '../../../store';
-import * as fromOrder from '../../store/reducers/order.reducer';
+import * as fromOrder from '../../store';
 import { SidenavComponent } from './sidenav.component';
 
 describe('SidenavComponent', () => {
   let component: SidenavComponent;
   let fixture: ComponentFixture<SidenavComponent>;
+  let store: MockStore<fromOrder.SideNavState>;
+  const state = {
+    order: {
+      selectedOrderId: 1,
+    },
+    orderSend: {
+      token: '',
+      orderSent: false,
+      error: '',
+    },
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [SidenavComponent],
-      providers: [PriceCalculatorService, SidenavService, SnackBarService],
+      providers: [
+        PriceCalculatorService,
+        SidenavService,
+        SnackBarService,
+        provideMockStore({ initialState: state }),
+      ],
       imports: [
         BrowserAnimationsModule,
         TranslateModule.forRoot(),
         RouterTestingModule,
         CoreModule,
-        StoreModule.forRoot(fromStore.reducers, {
-          runtimeChecks: {
-            strictStateImmutability: true,
-            strictActionImmutability: true,
-          },
-        }),
-        StoreModule.forFeature('sidenav', fromOrder.reducer),
       ],
     }).compileComponents();
+
+    store = TestBed.get<Store<fromOrder.SideNavState>>(Store);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SidenavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create', () => {
