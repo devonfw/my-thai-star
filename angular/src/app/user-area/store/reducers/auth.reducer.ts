baf3cce745cf84c573f2df275f23e9eb6312f2ad
Santos Jiménez Linares <sjimenez77@gmail.com>
2019-09-29 18:57:00 +0200

    Tests fixed and more refactoring

--- angular/src/app/user-area/store/reducers/auth.reducer.ts
+++ angular/src/app/user-area/store/reducers/auth.reducer.ts
@@ -1,6 +1,6 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { TokenString, UserDataResponse } from '../../models/user';
 import * as authActions from '../actions/auth.actions';
-import {TokenString, UserDataResponse} from '../../models/user';
 
 export interface AuthState {
   error: string | null;
@@ -18,18 +18,24 @@ export const initialState: AuthState = {
     logged: false,
   },
   token: {
-    token: ''
+    token: '',
   },
 };
 
 const authReducer = createReducer(
   initialState,
-  on(authActions.login, state => ({...state})),
-  on(authActions.loginSuccess, (state, { user }) => ({...state, user: user})),
-  on(authActions.token, (state, { token }) => ({...state, token: token})),
-  on(authActions.loginFail, (state, { error }) => ({...state, error: error.message})),
-  on(authActions.logout, state => ({...initialState})),
-  on(authActions.logoutFail, state => ({...state, error: 'Something went wrong !!!!.'})),
+  on(authActions.login, (state) => ({ ...state })),
+  on(authActions.loginSuccess, (state, { user }) => ({ ...state, user: user })),
+  on(authActions.token, (state, { token }) => ({ ...state, token: token })),
+  on(authActions.loginFail, (state, { error }) => ({
+    ...state,
+    error: error.message,
+  })),
+  on(authActions.logout, (state) => ({ ...initialState })),
+  on(authActions.logoutFail, (state) => ({
+    ...state,
+    error: 'Something went wrong !!!!.',
+  })),
 );
 
 export function reducer(state: AuthState | undefined, action: Action) {
