a02e3e8b1f2d99c1cdbaede484544b92eea17d31
Santos Jiménez Linares <sjimenez77@gmail.com>
2019-09-29 18:30:52 +0200

    More code refactored

--- angular/src/app/home/container/home/home.component.spec.ts
+++ angular/src/app/home/container/home/home.component.spec.ts
@@ -1,11 +1,13 @@
-import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { HomeComponent } from './home.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { TranslateModule } from '@ngx-translate/core';
+import { CoreModule } from '../../../core/core.module';
+import * as fromStore from '../../../store';
 import { HomeCardComponent } from '../../components/home-card/home-card.component';
 import { HomeLayoutComponent } from '../../components/home-layout/home-layout.component';
-import { CoreModule } from '../../../core/core.module';
-import { TranslateModule } from '@ngx-translate/core';
+import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -14,7 +16,18 @@ describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent, HomeCardComponent, HomeLayoutComponent],
-      imports: [TranslateModule.forRoot(), CoreModule, RouterTestingModule],
+      imports: [
+        TranslateModule.forRoot(),
+        CoreModule,
+        RouterTestingModule,
+        StoreModule.forRoot(fromStore.reducers, {
+          runtimeChecks: {
+            strictStateImmutability: true,
+            strictActionImmutability: true,
+          },
+        }),
+        EffectsModule.forRoot(fromStore.effects),
+      ],
     }).compileComponents();
   }));
 
