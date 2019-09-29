74c07159e6cd49fe7537d082ee960faffb058b9a
Santos Jiménez Linares <sjimenez77@gmail.com>
2019-09-29 18:57:00 +0200

    Tests fixed and more refactoring

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -4,8 +4,8 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { ElectronService, NgxElectronModule } from 'ngx-electron';
 import { AppComponent } from './app.component';
@@ -16,13 +16,26 @@ import { CoreModule } from './core/core.module';
 import { SnackBarService } from './core/snack-bar/snack-bar.service';
 import { WindowService } from './core/window/window.service';
 import { SidenavService } from './sidenav/services/sidenav.service';
-import * as fromStore from './store';
-import * as fromUser from './user-area/store';
 import { UserAreaService } from './user-area/services/user-area.service';
+import * as fromUser from './user-area/store';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let store: MockStore<fromUser.AppState>;
+
+  const state = {
+    error: null,
+    text: null,
+    user: {
+      user: '',
+      role: 'CUSTOMER',
+      logged: false,
+    },
+    token: {
+      token: '',
+    },
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -38,6 +51,7 @@ describe('AppComponent', () => {
         ConfigService,
         SidenavService,
         { provide: APP_BASE_HREF, useValue: '/' },
+        provideMockStore({ initialState: state }),
       ],
       imports: [
         BrowserAnimationsModule,
@@ -46,24 +60,20 @@ describe('AppComponent', () => {
         CoreModule,
         TranslateModule.forRoot(),
         NgxElectronModule,
-        EffectsModule.forRoot(fromStore.effects),
-        StoreModule.forRoot(fromStore.reducers, {
-          runtimeChecks: {
-            strictStateImmutability: true,
-            strictActionImmutability: true,
-          },
-        }),
-        EffectsModule.forFeature(fromUser.effects),
-        StoreModule.forFeature('auth', fromUser.reducers),
       ],
-    });
-    TestBed.compileComponents();
+    }).compileComponents();
+
+    store = TestBed.get<Store<fromUser.AppState>>(Store);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create', () => {
