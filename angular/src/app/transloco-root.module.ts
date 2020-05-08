349289da4b347e1118386657a53fcdd860685d36
Dattatreya <chillal.dattatreya@capgemini.com>
2020-05-08 13:03:14 +0530

    Added default languages

--- angular/src/app/transloco-root.module.ts
+++ angular/src/app/transloco-root.module.ts
@@ -25,7 +25,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
     {
       provide: TRANSLOCO_CONFIG,
       useValue: translocoConfig({
-        // availableLangs: ['en'],
+        availableLangs: ['en', 'es'],
         defaultLang: 'en',
         // Remove this option if your application doesn't support changing language in runtime.
         reRenderOnLangChange: true,
