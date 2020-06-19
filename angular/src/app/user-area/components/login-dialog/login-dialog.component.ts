b7081496ad63bf4aa36026b7c599b4f7f9a09657
Ravichandran M <ravichandran.m@capgemini.com>
2020-06-19 14:13:37 +0530

    login.component.ts

--- angular/src/app/user-area/components/login-dialog/login-dialog.component.ts
+++ angular/src/app/user-area/components/login-dialog/login-dialog.component.ts
@@ -1,28 +1,28 @@
-import { Component } from'@angular/core';
-import { MatDialogRef } from'@angular/material/dialog';
-import { FormGroup } from'@angular/forms';
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormGroup } from '@angular/forms';
 
 @Component({
-selector:'public-login-dialog',
-templateUrl:'./login-dialog.component.html',
-styleUrls: ['./login-dialog.component.scss'],
+  selector: 'public-login-dialog',
+  templateUrl: './login-dialog.component.html',
+  styleUrls: ['./login-dialog.component.scss'],
 })
-exportclassLoginDialogComponent {
+export class LoginDialogComponent {
 
-constructor(privatedialog:MatDialogRef<LoginDialogComponent>) { }
+  constructor(private dialog: MatDialogRef<LoginDialogComponent>) { }
 
-logInSubmit(formValue:FormGroup):void {
- this.dialog.close(formValue);
-}
+  logInSubmit(formValue: FormGroup): void {
+    this.dialog.close(formValue);
+  }
 
-signInSubmit(formValue:FormGroup):void {
- // Since there is no backend implementation for the signup below code is used to close dialog box after submission. 
- // Once the implementationis done uncomment the below code and remove the existing one.
- // this.dialog.close(formValue);
- this.dialog.close();
-}
+  signInSubmit(formValue: FormGroup): void {
+    // Since there is no backend implementation for the signup below code is used to close dialog box after submission.
+    // Once the implementationis done uncomment the below code and remove the existing one.
+    // this.dialog.close(formValue);
+    this.dialog.close();
+  }
 
-closeLoginDialog() {
- this.dialog.close();
-}
+  closeLoginDialog() {
+    this.dialog.close();
+  }
 }
