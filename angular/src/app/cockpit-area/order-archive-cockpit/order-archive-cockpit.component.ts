f251ceda4d5e5b3c37db813aa2c781ced481b44a
androx765 <androx765@gmail.com>
2021-04-22 15:44:58 +0200

    orderArchive-cockpit generated without content

---
+++ angular/src/app/cockpit-area/order-archive-cockpit/order-archive-cockpit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-archive-cockpit',
+  templateUrl: './order-archive-cockpit.component.html',
+  styleUrls: ['./order-archive-cockpit.component.scss']
+})
+export class OrderArchiveCockpitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
