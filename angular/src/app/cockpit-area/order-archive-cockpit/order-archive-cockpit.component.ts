7f41d4aad19ac7ffd58c8c26ac96dad1825e9637
androx765 <androx765@gmail.com>
2021-04-22 18:42:14 +0200

    adding OnDestroy and OnInit to order-archive, like order

--- angular/src/app/cockpit-area/order-archive-cockpit/order-archive-cockpit.component.ts
+++ angular/src/app/cockpit-area/order-archive-cockpit/order-archive-cockpit.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild  } from '@angular/core';
 
 @Component({
   selector: 'app-order-archive-cockpit',
   templateUrl: './order-archive-cockpit.component.html',
   styleUrls: ['./order-archive-cockpit.component.scss']
 })
-export class OrderArchiveCockpitComponent implements OnInit {
+export class OrderArchiveCockpitComponent implements OnInit, OnDestroy {
 
   constructor() { }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
   }
