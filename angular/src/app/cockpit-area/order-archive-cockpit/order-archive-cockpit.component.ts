5d303ba2b2fb0c90d8088431ae08cf9cd65d35b1
l.breitner <1926602@stud.hs-mannheim.de>
2021-04-23 19:32:33 +0200

    temp filling order-archive with order-cockpit

--- angular/src/app/cockpit-area/order-archive-cockpit/order-archive-cockpit.component.ts
+++ angular/src/app/cockpit-area/order-archive-cockpit/order-archive-cockpit.component.ts
@@ -1,18 +1,133 @@
-import { Component, OnDestroy, OnInit, ViewChild  } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Sort } from '@angular/material/sort';
+import { TranslocoService } from '@ngneat/transloco';
+import * as moment from 'moment';
+import { Subscription } from 'rxjs';
+import { ConfigService } from '../../core/config/config.service';
+import {
+  FilterCockpit,
+  Pageable,
+} from '../../shared/backend-models/interfaces';
+import { OrderListView } from '../../shared/view-models/interfaces';
+import { WaiterCockpitService } from '../services/waiter-cockpit.service';
+import { OrderDialogComponent } from '../order-cockpit/order-dialog/order-dialog.component';
 
 @Component({
-  selector: 'app-order-archive-cockpit',
+  selector: 'app-cockpit-order-archive-cockpit',
   templateUrl: './order-archive-cockpit.component.html',
-  styleUrls: ['./order-archive-cockpit.component.scss']
+  styleUrls: ['../order-cockpit/order-cockpit.component.scss'],
+  //uses order-cockpit scss, it should allways look the same!
 })
 export class OrderArchiveCockpitComponent implements OnInit, OnDestroy {
+  private translocoSubscription = Subscription.EMPTY;
+  private pageable: Pageable = {
+    pageSize: 8,
+    pageNumber: 0,
+    // total: 1,
+  };
+  private sorting: any[] = [];
 
-  constructor() { }
-  ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
+  pageSize = 8;
+
+  @ViewChild('pagingBar', { static: true }) pagingBar: MatPaginator;
+
+  orders: OrderListView[] = [];
+  totalOrders: number;
+
+  columns: any[];
+
+  displayedColumns: string[] = [
+    'booking.bookingDate',
+    'booking.email',
+    'booking.bookingToken',
+  ];
+
+  pageSizes: number[];
+
+  filters: FilterCockpit = {
+    bookingDate: undefined,
+    email: undefined,
+    bookingToken: undefined,
+  };
+
+  constructor(
+    private dialog: MatDialog,
+    private translocoService: TranslocoService,
+    private waiterCockpitService: WaiterCockpitService,
+    private configService: ConfigService,
+  ) {
+    this.pageSizes = this.configService.getValues().pageSizes;
   }
 
   ngOnInit(): void {
+    this.applyFilters();
+    this.translocoService.langChanges$.subscribe((event: any) => {
+      this.setTableHeaders(event);
+      moment.locale(this.translocoService.getActiveLang());
+    });
   }
 
+  setTableHeaders(lang: string): void {
+    this.translocoSubscription = this.translocoService
+      .selectTranslateObject('cockpit.table', {}, lang)
+      .subscribe((cockpitTable) => {
+        this.columns = [
+          { name: 'booking.bookingDate', label: cockpitTable.reservationDateH },
+          { name: 'booking.email', label: cockpitTable.emailH },
+          { name: 'booking.bookingToken', label: cockpitTable.bookingTokenH },
+        ];
+      });
+  }
+
+  applyFilters(): void {
+    this.waiterCockpitService
+      .getOrders(this.pageable, this.sorting, this.filters)
+      .subscribe((data: any) => {
+        if (!data) {
+          this.orders = [];
+        } else {
+          this.orders = data.content;
+        }
+        this.totalOrders = data.totalElements;
+      });
+  }
+
+  clearFilters(filters: any): void {
+    filters.reset();
+    this.applyFilters();
+    this.pagingBar.firstPage();
+  }
+
+  page(pagingEvent: PageEvent): void {
+    this.pageable = {
+      pageSize: pagingEvent.pageSize,
+      pageNumber: pagingEvent.pageIndex,
+      sort: this.pageable.sort,
+    };
+    this.applyFilters();
+  }
+
+  sort(sortEvent: Sort): void {
+    this.sorting = [];
+    if (sortEvent.direction) {
+      this.sorting.push({
+        property: sortEvent.active,
+        direction: '' + sortEvent.direction,
+      });
+    }
+    this.applyFilters();
+  }
+
+  selected(selection: OrderListView): void {
+    this.dialog.open(OrderDialogComponent, {
+      width: '80%',
+      data: selection,
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.translocoSubscription.unsubscribe();
+  }
 }
