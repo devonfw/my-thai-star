f2b80de26e9e12c3b0cb1e9f663d7b77ffcff7ce
androx765 <androx765@gmail.com>
2021-05-17 17:51:03 +0200

    added admin cockpit

--- angular/src/app/cockpit-area/admin-cockpit/admin-cockpit.component.spec.ts
+++ angular/src/app/cockpit-area/admin-cockpit/admin-cockpit.component.spec.ts
@@ -1,22 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from '../../core/core.module';
+import { WaiterCockpitModule } from '../cockpit.module';
+import { AdminService } from '../services/admin.service';
 import { AdminCockpitComponent } from './admin-cockpit.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { ConfigService } from '../../core/config/config.service';
+import { config } from '../../core/config/config';
+import { getTranslocoModule } from '../../transloco-testing.module';
 
-describe('AdminComponent', () => {
+fdescribe('AdminCockpitComponent', () => {
   let component: AdminCockpitComponent;
-  let fixture: ComponentFixture<AdminCockpitComponent>;
+  let initialState;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AdminCockpitComponent ]
-    })
-    .compileComponents();
+    initialState = { config };
+   
   }));
-
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdminCockpitComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      providers: [
+        AdminService,
+        provideMockStore({ initialState }),
+        ConfigService,
+        HttpClient,
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        WaiterCockpitModule,
+        getTranslocoModule(),
+        CoreModule,
+        HttpClientTestingModule,
+      ],
+    }).compileComponents();
+  component = TestBed.inject(AdminCockpitComponent);
   });
 
   it('should create', () => {
