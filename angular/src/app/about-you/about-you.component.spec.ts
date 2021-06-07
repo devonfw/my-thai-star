e67b9d2ca42fc81571240e12cc3c69ef89b89fb1
simon <simonschiebler@gmail.com>
2021-06-07 14:02:26 +0200

    made tests pass

--- angular/src/app/about-you/about-you.component.spec.ts
+++ angular/src/app/about-you/about-you.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { getTranslocoModule } from '../transloco-testing.module';
 import { AboutYouComponent } from './about-you.component';
 
 describe('AboutYouComponent', () => {
@@ -8,7 +8,8 @@ describe('AboutYouComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AboutYouComponent ]
+      declarations: [ AboutYouComponent ],
+      imports:[getTranslocoModule()]
     })
     .compileComponents();
   }));
