4c2584b96ade0737b53c9b7b7cf96b29a25624b7
HeiwaJan <jan.berger@stud.hs-mannheim.de>
2021-06-14 14:51:44 +0200

    Made Order State Request with Alexa work

--- alexa/lambda/config.js
+++ alexa/lambda/config.js
@@ -2,6 +2,8 @@ module.exports = {
     myThaiStarBackend:{
         host:"java",
         port:8081,
-        createBookingEndpoint:"/mythaistar/services/rest/bookingmanagement/v1/booking"
+        createBookingEndpoint:"/mythaistar/services/rest/bookingmanagement/v1/booking",
+        createOrderEndpoint:"/mythaistar/services/rest/ordermanagement/v1/order",
+        getActiveOrdersEndpoint:"/mythaistar/services/rest/ordermanagement/v1/order/activeOrders"
     }
 }
